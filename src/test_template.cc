939d0be40c1a80405458f8d9fad9c1e2134a5d90
Dan Gordon <dexter.urbane@gmail.com>
2013-02-15 14:52:04 +1100

    Clean up code.

--- src/test_template.cc
+++ src/test_template.cc
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE test_template
 #include <boost/test/included/unit_test.hpp>
 
-BOOST_AUTO_TEST_SUITE (test_template) // name of the test suite is stringtest
+BOOST_AUTO_TEST_SUITE (test_template) // Name of test suite is test_template.
 
 BOOST_AUTO_TEST_CASE (test1)
 {
@@ -10,8 +10,8 @@ BOOST_AUTO_TEST_CASE (test1)
 
 BOOST_AUTO_TEST_CASE (test2)
 {
-  BOOST_REQUIRE_EQUAL (1, 1); // basic test 
-  BOOST_REQUIRE_EQUAL (1, 0); // basic test 
+  BOOST_REQUIRE_EQUAL (1, 1);
+  BOOST_REQUIRE_EQUAL (1, 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
