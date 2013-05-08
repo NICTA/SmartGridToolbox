812046255089c0c77c5b756c288b5db4f50b794c
Dan Gordon <dexter.urbane@gmail.com>
2013-05-08 15:20:19 +1000

    Change interface for properties.

--- src/Property.h
+++ src/Property.h
@@ -9,6 +9,13 @@
 
 namespace SmartGridToolbox
 {
+   enum class PropertyAccess
+   {
+      GET,
+      SET,
+      BOTH
+   };
+
    class PropertyBase
    {
       public:
@@ -18,35 +25,18 @@ namespace SmartGridToolbox
          }
    };
 
-   template <typename T> class ReadProperty : public virtual PropertyBase
+   template <typename T, PropertyAccess access> class Property;
+
+   template <typename T> class Property<T, PropertyAccess::GET> : 
+      public virtual PropertyBase
    {
       public:
-         template<typename G> ReadProperty(G get) : getFunction_(get)
+         template<typename G> Property(G get) : getFunction_(get)
          {
             // Empty.
          }
 
-         template<typename G> void init(G get)
-         {
-            getFunction_ = std::function<T ()>(get);
-         }
-
-         void bindTo(ReadProperty<T> & to)
-         {
-            getFunction_ = to.getFunction_;
-         }
-
          T get() const
-         {
-            return getFunction_();
-         }
-
-         T operator()() const
-         {
-            return getFunction_();
-         }
-
-         operator T () const
          {
             return getFunction_();
          }
@@ -55,10 +45,11 @@ namespace SmartGridToolbox
          std::function<T ()> getFunction_;
    };
 
-   template <typename T> class WriteProperty : public virtual PropertyBase
+   template <typename T> class Property<T, PropertyAccess::SET> : 
+      public virtual PropertyBase
    {
       public:
-         template<typename S> WriteProperty(S set) : 
+         template<typename S> Property(S set) : 
             setFunction_(set)
          {
             // Empty.
@@ -69,21 +60,18 @@ namespace SmartGridToolbox
             return setFunction_(rhs);
          }
 
-         T operator=(const T & rhs) const
-         {
-            return setFunction_(rhs);
-         }
-
       private:
          std::function<void(const T &)> setFunction_;
    };
 
-   template <typename T> class ReadWriteProperty : 
-      public ReadProperty<T>, public WriteProperty<T>
+   template <typename T> class Property<T, PropertyAccess::BOTH> : 
+      public Property<T, PropertyAccess::GET>,
+      public Property<T, PropertyAccess::SET>
    {
       public:
-         template<typename G, typename S> ReadWriteProperty(G get, S set) : 
-            ReadProperty<T>(get), WriteProperty<T>(set)
+         template<typename G, typename S> Property(G get, S set) : 
+            Property<T, PropertyAccess::GET>(get),
+            Property<T, PropertyAccess::SET>(set)
          {
             // Empty.
          }
