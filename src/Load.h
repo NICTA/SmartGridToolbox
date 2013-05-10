d12b79dfd193ccec4a9da865f42eed72498f0c5d
Dan Gordon <dexter.urbane@gmail.com>
2013-05-10 12:11:55 +1000

    Oops, did something silly with Load ABC.

--- src/Load.h
+++ src/Load.h
@@ -6,25 +6,11 @@
 namespace SmartGridToolbox
 
 {
-   class Load : public Component
+   class Load
    {
-      // Inherited, overridden functions: from Component.
-      public:
-         virtual ptime getValidUntil() const override {return getTime() + dt_;}
-
-      private:
-         virtual void initializeState(ptime t) override;
-         virtual void updateState(ptime t0, ptime t1) override;
-
-      // Inherited, overridden functions: from Load.
       public:
          virtual Complex getPower() override {return Complex(Ph_, 0.0);}
-      public:
-         virtual ~Load() {}
-         virtual Complex getPower() = 0;
    };
-
-   class LoadComponent : public Component, public Load {};
 }
 
 #endif // LOAD_DOT_H
