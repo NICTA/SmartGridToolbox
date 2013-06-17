75af1c36dafbada0160c53d240a62d1677a23d7d
Dan Gordon <dexter.urbane@gmail.com>
2013-06-17 21:37:42 +1000

    Safeguard against zero dimensions in some sub arrays.

---
+++ experimental/MATLAB_LOADFLOW/my_1PQ.m
@@ -0,0 +1,12 @@
+% Bus Data
+%	ID	TYPE	P	Q	M	theta	gs	bs
+busdata = [ ...
+	1   	1   	0.1000	0.0000	NA	NA	0.0000	0.0000
+	2   	0   	NA	NA	1.0500	0.0000	0.0000	0.0000
+];
+
+% Branch Data
+%	FROM	TO	g	b
+branchdata = [ ...
+	1   	2   	2.0000	-4.0000
+];
