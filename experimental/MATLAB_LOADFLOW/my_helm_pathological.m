768a379b3582ead42be6dca2fd57c319b737b0ca
Dan Gordon <dexter.urbane@gmail.com>
2013-06-19 12:56:28 +1000

    Add pathological case for helm.
    It appears that having a radiall network is not good for helm. This is
    possibly due to condition number of A.

---
+++ experimental/MATLAB_LOADFLOW/my_helm_pathological.m
@@ -0,0 +1,18 @@
+% Bus Data
+%	ID	TYPE	P	Q	M	theta	gs	bs	IcR	IcI
+busdata = [ ...
+	1	1	0.2	0	NaN	NaN	0	0	0	0
+	2	1	0.2	0	NaN	NaN	0	0	0	0
+	3	2	0.2	NaN	1.1000	NaN	0	0	0	0
+	4	2	0.2	NaN	1.1000	NaN	0	0	0	0
+	5	3	NaN	NaN	1	0	0	0	0	0
+];
+
+% Branch Data
+%	FROM	TO	g	b
+branchdata = [ ...
+	1	2	5	-1
+	2	3	2	-1
+	3	4	7	-1
+	4	5	3	-1
+];
