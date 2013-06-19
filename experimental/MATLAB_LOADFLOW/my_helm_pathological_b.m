8ec3241d0d5974cb3a957fb827621f2ef03271f8
Dan Gordon <dexter.urbane@gmail.com>
2013-06-19 20:39:35 +1000

    Another slightly pathological case involving a single dimension..

---
+++ experimental/MATLAB_LOADFLOW/my_helm_pathological_b.m
@@ -0,0 +1,12 @@
+% Bus Data
+%	ID	TYPE	P	Q	M	theta	gs	bs	IcR	IcI
+busdata = [ ...
+	2	2	0.2	NaN	1	NaN	0	0	0	0
+	3	3	NaN	NaN	1	0	0	0	0	0
+];
+
+% Branch Data
+%	FROM	TO	g	b
+branchdata = [ ...
+	2	3	18	-1
+];
