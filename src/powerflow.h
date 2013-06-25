089a91c92c8105e2c0a3c6ce669627cb6348914b
Dan Gordon <dexter.urbane@gmail.com>
2013-06-25 11:24:06 +1000

    Fix comment formatting.

--- src/powerflow.h
+++ src/powerflow.h
@@ -6,7 +6,6 @@
  @{
  **/
 
-
 #ifndef _POWERFLOW_H
 #define _POWERFLOW_H
 
@@ -14,10 +13,10 @@
 #include "solver_nr.h"
 
 #ifdef _POWERFLOW_CPP
-#define GLOBAL
+#define
 #define INIT(A) = (A)
 #else
-#define GLOBAL extern
+#define extern
 #define INIT(A)
 #endif
 
@@ -25,78 +24,52 @@
 void print_matrix(complex mat[3][3]);
 #endif
 
-#define GETOBJECT(obj) ((OBJECT *) obj - 1)
-#define IMPORT_CLASS(name) extern CLASS *name##_class
-
-typedef enum {SM_FBS=0, SM_GS=1, SM_NR=2} SOLVERMETHOD; /**< powerflow solver methodology */
-typedef enum {MM_SUPERLU=0, MM_EXTERN=1} MATRIXSOLVERMETHOD;  /**< NR matrix solver methodlogy */
-
-//Structure to hold external LU solver calls
-typedef struct s_ext_fxn {
-	void *dllLink;
-	void *ext_init;
-	void *ext_alloc;
-	void *ext_solve;
-	void *ext_destroy;
-} EXT_LU_FXN_CALLS;
-
-GLOBAL char256 LUSolverName INIT("");                /**< filename for external LU solver */
-GLOBAL EXT_LU_FXN_CALLS LUSolverFcns;                /**< links to external LU solver functions */
-GLOBAL SOLVERMETHOD solver_method INIT(SM_FBS);      /**< powerflow solver methodology */
-GLOBAL bool use_line_cap INIT(false);                /**< Flag to include line capacitance quantities */
-GLOBAL bool use_link_limits INIT(true);              
-   /**< Flag to include line/transformer ratings and provide a warning if exceeded */
-GLOBAL MATRIXSOLVERMETHOD matrix_solver_method INIT(MM_SUPERLU);
-   /**< Newton-Raphson uses superLU as the default solver */
-GLOBAL unsigned int NR_bus_count INIT(0);
-   /**< Newton-Raphson bus count - used for determining size of bus vector */
-GLOBAL unsigned int NR_branch_count INIT(0);         
-   /**< Newton-Raphson branch count - used for determining size of branch vector */
-GLOBAL BUSDATA *NR_busdata INIT(NULL);               /**< Newton-Raphson bus data pointer array */
-GLOBAL BRANCHDATA *NR_branchdata INIT(NULL);         /**< Newton-Raphson branch data pointer array */
-GLOBAL double *deltaI_NR INIT(NULL);                 /**< Newton-Raphson current differences pointer array */
-GLOBAL int NR_curr_bus INIT(-1);                     
-   /**< Newton-Raphson current bus indicator - used to populate NR_busdata */
-GLOBAL int NR_curr_branch INIT(-1);                  
-   /**< Newton-Raphson current branch indicator - used to populate NR_branchdata */
-GLOBAL int64 NR_iteration_limit INIT(500);           /**< Newton-Raphson iteration limit (per GridLAB-D iteration) */
-GLOBAL bool NR_cycle INIT(true);                     
-   /**< Newton-Raphson pass indicator - false = solution pass, true = metering/accumulation pass */
-GLOBAL bool NR_admit_change INIT(true);              /**< Newton-Raphson admittance matrix change detector. */
- ///< Used to prevent complete recalculation of admittance at every timestep */
-GLOBAL int NR_superLU_procs INIT(1);                 
-   /**< Newton-Raphson related - superLU MT processor count to request - separate from thread_count */
-GLOBAL TIMESTAMP NR_retval INIT(TS_NEVER);           
-   /**< Newton-Raphson current return value - if t0 objects know we aren't going anywhere */
-GLOBAL OBJECT *NR_swing_bus INIT(NULL);              /**< Newton-Raphson swing bus */
-GLOBAL bool FBS_swing_set INIT(false);               /**< Forward-Back Sweep swing assignment variable */
-GLOBAL bool show_matrix_values INIT(false);          /**< flag to enable dumping matrix calculations as they occur */
-GLOBAL double primary_voltage_ratio INIT(60.0);      
-   /**< primary voltage ratio (@todo explain primary_voltage_ratio in powerflow (ticket #131) */
-GLOBAL double nominal_frequency INIT(60.0);          /**< nomimal operating frequencty */
-GLOBAL double warning_underfrequency INIT(55.0);     /**< frequency below which a warning is posted */
-GLOBAL double warning_overfrequency INIT(65.0);      /**< frequency above which a warning is posted */
-GLOBAL double nominal_voltage INIT(240.0);           /**< nominal voltage level */
-GLOBAL double warning_undervoltage INIT(0.8);        
-   /**< voltage magnitude (per unit) below which a warning is posted */
-GLOBAL double warning_overvoltage INIT(1.2);         
-   /**< voltage magnitude (per unit) above which a warning is posted */
-GLOBAL double warning_voltageangle INIT(2.0);        /**< voltage angle (over link) above which a warning is posted */
-GLOBAL bool require_voltage_control INIT(false);     /**< flag to enable voltage control source requirement */
-GLOBAL double geographic_degree INIT(0.0);           /**< topological degree factor */
-GLOBAL complex fault_Z INIT(complex(1e-6,0));        /**< fault impedance */
-GLOBAL double default_maximum_voltage_error INIT(1e-6); /**< default sync voltage convergence limit [puV] */
-GLOBAL double default_maximum_power_error INIT(0.0001); /**< default power convergence limit for multirun */
-GLOBAL OBJECT *restoration_object INIT(NULL);        /**< restoration object of the system */
-GLOBAL OBJECT *fault_check_object INIT(NULL);        /**< fault_check object of the system */
+bool use_line_cap INIT(false);         /**< Flag to include line capacitance quantities */
+bool use_link_limits INIT(true);       /**< Flag to include line/transformer ratings and provide a warning if exceeded */
+MATRIXSOLVERMETHOD matrix_solver_method INIT(MM_SUPERLU);   /**< Newton-Raphson uses superLU as the default solver */
+unsigned int NR_bus_count INIT(0);     /**< Newton-Raphson bus count - used for determining size of bus vector */
+unsigned int NR_branch_count INIT(0);  /**< Newton-Raphson branch count - used for determining size of branch vector */
+BUSDATA *NR_busdata INIT(NULL);        /**< Newton-Raphson bus data pointer array */
+BRANCHDATA *NR_branchdata INIT(NULL);  /**< Newton-Raphson branch data pointer array */
+double *deltaI_NR INIT(NULL);          /**< Newton-Raphson current differences pointer array */
+int NR_curr_bus INIT(-1);              /**< Newton-Raphson current bus indicator - used to populate NR_busdata */
+int NR_curr_branch INIT(-1);           /**< Newton-Raphson current branch indicator - used to populate NR_branchdata */
+int64 NR_iteration_limit INIT(500);    /**< Newton-Raphson iteration limit (per GridLAB-D iteration) */
+bool NR_cycle INIT(true);              /**< Newton-Raphson pass indicator 
+                                        *   false = solution pass, true = metering/accumulation pass */
+bool NR_admit_change INIT(true);       /**< Newton-Raphson admittance matrix change detector.
+                                        *   Used to prevent complete recalculation of admittance at every timestep */
+int NR_superLU_procs INIT(1);          /**< Newton-Raphson related - superLU MT processor count to request
+                                        *   separate from thread_count */
+TIMESTAMP NR_retval INIT(TS_NEVER);    /**< Newton-Raphson current return value
+                                        *   if t0 objects know we aren't going anywhere */
+OBJECT *NR_swing_bus INIT(NULL);       /**< Newton-Raphson swing bus */
+bool FBS_swing_set INIT(false);        /**< Forward-Back Sweep swing assignment variable */
+bool show_matrix_values INIT(false);   /**< flag to enable dumping matrix calculations as they occur */
+double primary_voltage_ratio INIT(60.0);  /**< primary voltage ratio
+                                           *   (@todo explain primary_voltage_ratio in powerflow (ticket #131) */
+double nominal_frequency INIT(60.0);   /**< nomimal operating frequencty */
+double warning_underfrequency INIT(55.0); /**< frequency below which a warning is posted */
+double warning_overfrequency INIT(65.0);  /**< frequency above which a warning is posted */
+double nominal_voltage INIT(240.0);    /**< nominal voltage level */
+double warning_undervoltage INIT(0.8); /**< voltage magnitude (per unit) below which a warning is posted */
+double warning_overvoltage INIT(1.2);  /**< voltage magnitude (per unit) above which a warning is posted */
+double warning_voltageangle INIT(2.0); /**< voltage angle (over link) above which a warning is posted */
+bool require_voltage_control INIT(false); /**< flag to enable voltage control source requirement */
+double geographic_degree INIT(0.0);    /**< topological degree factor */
+complex fault_Z INIT(complex(1e-6,0)); /**< fault impedance */
+double default_maximum_voltage_error INIT(1e-6);   /**< default sync voltage convergence limit [puV] */
+double default_maximum_power_error INIT(0.0001);   /**< default power convergence limit for multirun */
+OBJECT *restoration_object INIT(NULL); /**< restoration object of the system */
+OBJECT *fault_check_object INIT(NULL); /**< fault_check object of the system */
 
 /* used by many powerflow enums */
 #define UNKNOWN 0
-#define ROUNDOFF 1e-6                                // Numerical accuracy for zero in float comparisons
+#define ROUNDOFF 1e-6                                      /  Numerical accuracy for zero in float comparisons
 
 
 #include "powerflow_object.h"
 
-#endif                                               // _POWERFLOW_H
+#endif                                                     /  _POWERFLOW_H
 
 /**@}*/
