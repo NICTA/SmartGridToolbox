692c6ce6564066282e5abab02ae38f050b74fe4d
Dan Gordon <dexter.urbane@gmail.com>
2013-07-12 10:12:25 +1000

    Add balanced versions, to be modified.

---
+++ src/BalancedPowerFlowNR.cc
@@ -0,0 +1,162 @@
+#include "PowerFlowNR.h"
+#include "Output.h"
+
+namespace SmartGridToolbox
+{
+   void PowerFlowNR::addBus(NRBus * bus)
+   {
+      switch (bus->type)
+      {
+         case BusType::SL :
+            SLBusses_.push_back(bus);
+            break;
+         case BusType::PQ : 
+            PQBusses_.push_back(bus);
+            break;
+         case BusType::PV : 
+            error("PV busses are not supported yet.");
+            break;
+      }
+   }
+
+   void PowerFlowNR::validate()
+   {
+      // Determine sizes:
+      nSL_ = SLBusses_.size();
+      nPQ_ = PQBusses_.size();
+      assert(nSL_ == 1); // May change in future...
+      assert(nPQ_ > 0); // May change in future...
+      nBus_ = nSL_ + nPQ_;
+      nTerm_ = 3 * nBus_;
+      nVar_ = 6 * nPQ_;
+      nVarD2_ = 3 * nPQ_;
+
+      // Size all arrays:
+      PPQ_.resize(nTerm_, false);
+      QPQ_.resize(nTerm_, false);
+      Vr_.resize(nTerm_, false);
+      Vi_.resize(nTerm_, false);
+      Y_.resize(nTerm_, nTerm_, false);
+      G_.resize(nTerm_, nTerm_, false);
+      B_.resize(nTerm_, nTerm_, false);
+      x_.resize(nVar_, false);
+      f_.resize(nVar_, false);
+      J_.resize(nVar_, nVar_, false);
+
+      // Insert PQ and PV busses into list of all busses. 
+      busses_ = BusVec(); // Clear contents...
+      busses_.reserve(nBus_);
+      busses_.insert(busses_.end(), PQBusses_.begin(), PQBusses_.end());
+      busses_.insert(busses_.end(), SLBusses_.begin(), SLBusses_.end());
+
+      // Index all PQ busses:
+      // TODO: remove if not needed.
+      for (int i = 0; i < nPQ_; ++i)
+      {
+         PQBusses_[i]->idxPQ = i;
+      }
+
+      // Set the slack voltages:
+      V0_ = SLBusses_[0]->V; // Array copy.
+
+      // Set the PPQ_ and QPQ_ arrays of real and reactive power on each terminal:
+      for (int i = 0; i < nPQ_; ++i)
+      {
+         for (int k = 0; k < 3; ++k)
+         {
+            PPQ_(3 * i + k) = busses_[i]->P[k];
+            QPQ_(3 * i + k) = busses_[i]->Q[k];
+         }
+      }
+
+      // Build the bus admittance matrix:
+      buildBusAdmit();
+
+      // And set G_ and B_:
+      G_ = real(Y_);
+      B_ = imag(Y_);
+   }
+
+   void PowerFlowNR::buildBusAdmit()
+   {
+      int ntot = 3 * (nPQ_ + nSL_);
+      for (const NRBranch * const branch : branches_)
+      {
+         const NRBus * busi = branch->busi;
+         const NRBus * busk = branch->busk;
+
+         int ibus = busi->idxPQ;
+         int kbus = busk->idxPQ;
+
+         int idxi = 3 * ibus;
+         int idxk = 3 * kbus;
+         for (int p1 = 0; p1 < 3; ++p1)
+         {
+            for (int p2 = 0; p2 < 3; ++p2)
+            {
+               Y_(idxi + p1, idxi + p2) += branch->Y[p1][p2];
+               Y_(idxk + p1, idxk + p2) += branch->Y[p1 + 3][p2 + 3];
+               Y_(idxi + p1, idxk + p2) += branch->Y[p1][p2 + 3];
+               Y_(idxk + p1, idxi + p2) += branch->Y[p1 + 3][p2];
+            }
+         }
+      }
+   }
+
+   void PowerFlowNR::initx()
+   {
+      for (int i = 0; i < nPQ_; ++i)
+      {
+         const NRBus & bus = *busses_[i + 1]; 
+         for (int k = 0; k < 3; ++k)
+         {
+            x_(3 * i + k) = V0_[i].real();
+            x_(3 * nPQ_ + 3 * i + k) = V0_[i].imag();
+         }
+      }
+   }
+
+   void PowerFlowNR::updateF()
+   {
+      using namespace ublas;
+      typedef vector_range<vector<double>> VRD; 
+
+      range rPQ(0, 3 * nPQ_);
+      range rAll(0, 3 * nPQ_ + 3);
+      range r1(0, 3 * nPQ_);
+      range r2(3 * nPQ_, 6 * nPQ_);
+
+      VRD x1(x_, r1);
+      VRD x2(x_, r2);
+
+      VRD(Vr_, rPQ) = VRD(x_, r1);
+      for (int i = 0; i < 3; ++i) Vr_(nPQ_) = V0_.real();
+      VRD(Vi, rPQ) = VRD(x_, r2);
+      Vi(nPQ_) = V0_.imag();
+
+      ublas::vector<double> M2 = element_prod(Vr, Vr) + element_prod(Vi, Vi); 
+
+      matrix_range<matrix<double>> Grng(G, rPQ, rAll); 
+      matrix_range<matrix<double>> Brng(G, rPQ, rAll); 
+
+      ublas::vector<double> DR = element_div((-element_prod(PPQ_, x1) - element_prod(QPQ_, x2)), M2)
+                               + prod(Grng, Vr) - prod(Brng, Vi);
+      ublas::vector<double> DI = element_div((-element_prod(PPQ_, x2) + element_prod(QPQ_, x1)), M2)
+                               + prod(Grng, Vi) + prod(Brng, Vr);
+
+      VRD(f_, r1) = DR;
+      VRD(f_, r2) = DI;
+   }
+
+   void PowerFlowNR::solve()
+   {
+      const double tol = 1e-20;
+      const int maxiter = 20;
+      initx();
+      for (int i = 0; i < maxiter; ++ i)
+      {
+         calcF
+
+      }
+   }
+}
---
+++ src/BalancedPowerFlowNR.h
@@ -0,0 +1,99 @@
+#ifndef NR_LOAD_FLOW_DOT_H
+#define NR_LOAD_FLOW_DOT_H
+
+#include "Common.h"
+#include "PowerFlow.h"
+#include <vector>
+
+namespace SmartGridToolbox
+{
+   class NRBus
+   {
+      public:
+         int id;                       ///< Arbitrary bus ID, for external use.
+         BusType type;                 ///< bus type (0=PQ, 1=PV, 2=SWING).
+
+         std::array<Complex, 3> V;     ///< NRBus voltage / phase.
+         std::array<Complex, 3> Y;     ///< Constant admittance/phase.
+         std::array<Complex, 3> I;     ///< Constant current / phase.
+         std::array<Complex, 3> S;     ///< Constant power / phase.
+
+         std::array<double, 3> P;      ///< Real power injection.
+         std::array<double, 3> Q;      ///< Reactive power injection.
+
+         int idxPQ;                    ///< My index in list of PQ busses.
+   };
+
+   class NRBranch
+   {
+      public:
+         Array2D<Complex, 6, 6> Y;     ///< Complex value of elements in bus admittance matrix in NR solver.
+         const NRBus * busi;           ///< My i bus.
+         const NRBus * busk;           ///< My k bus.
+   };
+
+   class PowerFlowNR
+   {
+      public:
+         typedef std::vector<NRBus *> BusVec;
+         typedef std::vector<NRBranch *> BranchVec;
+      public:
+         void addBus(NRBus * bus);
+         void addBranch(NRBranch * branch)
+         {
+            branches_.push_back(branch);
+         }
+         void validate();
+         void solve();
+      private:
+         void buildBusAdmit();
+         void initx();
+         void updateF();
+      private:
+         /// @name Vectors of busses and branches.
+         /// @{
+         BusVec busses_;
+         BusVec SLBusses_;
+         BusVec PQBusses_;
+
+         BranchVec branches_;
+         /// @}
+
+         /// @name Array bounds.
+         /// @{
+         int nSL_;                     ///< Number of slack busses.
+         int nPQ_;                     ///< Number of PQ busses.
+         int nBus_;                    ///< Total number of busses.
+         int nTerm_;                   ///< Total number of bus terminals (each bus has 1 terminal per phase).
+         int nVar_;                    ///< Number of variables e.g. length of x_ matrix in NR algorithm.
+         int nVarD2_;                  ///< Half nVar_.
+         /// @}
+
+         /// @name ublas ranges into vectors/matrices.
+         /// @{
+         ublas::range rTermPQ(0, 3 * nPQ_);
+         ublas::range rTermAll(0, 3 * nPQ_ + 3);
+         ublas::range rx1(0, 3 * nPQ_);
+         ublas::range rx2(3 * nPQ_, 6 * nPQ_);
+         /// @}
+
+         std::array<Complex, 3> V0_;   ///< Slack voltages.
+
+         /// @name ublas ranges into vectors/matrices.
+         /// @{
+         ublas::vector<double> PPQ_;   ///< Total power injection of PQ busses.
+         ublas::vector<double> QPQ_;
+
+         ublas::vector<double> Vr_;
+         ublas::vector<double> Vi_;
+         ublas::compressed_matrix<Complex> Y_;
+         ublas::compressed_matrix<double> G_;
+         ublas::compressed_matrix<double> B_;
+
+         ublas::vector<double> x_;
+         ublas::vector<double> f_;
+         ublas::compressed_matrix<double> J_;
+   };
+}
+
+#endif // NR_LOAD_FLOW_DOT_H
