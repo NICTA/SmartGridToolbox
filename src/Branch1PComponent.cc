76693631f7e4f71cc3fee14fc4f66f8af473193d
Dan Gordon <dexter.urbane@gmail.com>
2013-07-18 11:00:43 +1000

    Oops, forgot to add new files.

---
+++ src/Branch1PComponent.cc
@@ -0,0 +1,58 @@
+#include "Branch1PComponent.h"
+#include "Bus1PComponent.h"
+#include "Model.h"
+#include "Network1PComponent.h"
+ 
+namespace SmartGridToolbox
+{
+   void Branch1PComponentParser::parse(const YAML::Node & nd, Model & mod) const
+   {
+      Branch1PComponent * comp = new Branch1PComponent;
+      assertFieldPresent(nd, "name");
+      const std::string nameStr = nd["name"].as<std::string>();
+      comp->setName(nameStr);
+
+      assertFieldPresent(nd, "Y_");
+
+      mod.addComponent(*comp);
+   }
+
+   void Branch1PComponentParser::postParse(const YAML::Node & nd, Model & mod) const
+   {
+      const std::string compNameStr = nd["name"].as<std::string>();
+      Branch1PComponent * comp = mod.getComponentNamed<Branch1PComponent>(compNameStr);
+
+      const std::string networkStr = nd["network"].as<std::string>();
+      Network1PComponent * networkComp = mod.getComponentNamed<Network1PComponent>(networkStr);
+      if (networkComp != nullptr)
+      {
+         comp->setNetwork(*networkComp);
+      }
+      else
+      {
+         error("For component %s, network %s was not found in the model.");
+      }
+
+      const std::string busiStr = nd["bus_i"].as<std::string>();
+      Bus1PComponent * busiComp = mod.getComponentNamed<Bus1PComponent>(busiStr);
+      if (networkComp != nullptr)
+      {
+         comp->setBusi(*busiComp);
+      }
+      else
+      {
+         error("For component %s, bus_i %s was not found in the model.");
+      }
+
+      const std::string buskStr = nd["bus_k"].as<std::string>();
+      Bus1PComponent * buskComp = mod.getComponentNamed<Bus1PComponent>(buskStr);
+      if (networkComp != nullptr)
+      {
+         comp->setBusk(*buskComp);
+      }
+      else
+      {
+         error("For component %s, bus_k %s was not found in the model.");
+      }
+   }
+}
---
+++ src/Branch1PComponent.h
@@ -0,0 +1,78 @@
+#ifndef BRANCH_1P_COMPONENT_DOT_H
+#define BRANCH_1P_COMPONENT_DOT_H
+
+#include "Common.h"
+#include "Component.h"
+#include "Parser.h"
+#include "PowerFlow.h"
+
+#include <iostream>
+
+namespace SmartGridToolbox
+{
+   class Network1PComponent;
+   class Bus1PComponent;
+
+   class Branch1PComponentParser : public ComponentParser
+   {
+      public:
+         static constexpr const char * getComponentName()
+         {
+            return "branch_1_phase";
+         }
+
+      public:
+         virtual void parse(const YAML::Node & nd, Model & mod) const override;
+
+         virtual void postParse(const YAML::Node & nd, Model & mod) const override;
+   };
+
+   class Branch1PComponent : public Component
+   {
+      /// @name Public overridden functions: from Component.
+      /// @{
+      public:
+         virtual ptime getValidUntil() const override 
+         {
+            return pos_infin;
+         }
+      /// @}
+
+      /// @name Private overridden functions: from Component.
+      /// @{
+      private:
+         virtual void initializeState(ptime t) override {};
+         virtual void updateState(ptime t0, ptime t1) override {};
+      /// @}
+
+      /// @name My public member functions.
+      /// @{
+      public:
+         const Network1PComponent & getNetwork() const {return *network_;}
+         Network1PComponent & getNetwork() {return *network_;}
+         void setNetwork(Network1PComponent & network) {network_ = &network;}
+
+         const Array2D<Complex, 2, 2> & getY() const {return Y_;}
+         Array2D<Complex, 2, 2> & getY() {return Y_;}
+
+         const Bus1PComponent & getBusi() const {return *busi_;}
+         Bus1PComponent & getBusi() {return *busi_;}
+         void setBusi(Bus1PComponent & busi) {busi_ = &busi;}
+
+         const Bus1PComponent & getBusk() const {return *busk_;}
+         Bus1PComponent & getBusk() {return *busk_;}
+         void setBusk(Bus1PComponent & busk) {busk_ = &busk;}
+      /// @}
+
+      /// @name My private member variables.
+      /// @{
+      private:
+         Network1PComponent * network_;   ///< Network.
+         Array2D<Complex, 2, 2> Y_;       ///< Complex value of elements in bus admittance matrix in NR solver.
+         Bus1PComponent * busi_;          ///< My i bus.
+         Bus1PComponent * busk_;          ///< My k bus.
+      /// @}
+   };
+}
+
+#endif // BRANCH_1P_COMPONENT_DOT_H
