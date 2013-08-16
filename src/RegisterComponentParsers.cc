2d07697419bc2cfd433359922c4dd23775c4c5ba
Dan Gordon <dan.gordon@nicta.com.au>
2013-08-16 10:54:08 +1000

    Removing 1P stuff.

--- src/RegisterComponentParsers.cc
+++ src/RegisterComponentParsers.cc
@@ -1,26 +1,18 @@
 #include "RegisterComponentParsers.h"
-#include "Branch1P.h"
 #include "Branch.h"
-#include "Bus1P.h"
 #include "Bus.h"
-#include "Network1P.h"
 #include "Network.h"
 #include "TestComponent.h"
-#include "ZipToGround1P.h"
 #include "ZipToGround.h"
 
 namespace SmartGridToolbox
 {
    void registerComponentParsers(Parser & p)
    {
-      p.registerComponentParser<Branch1PParser>();
       p.registerComponentParser<BranchParser>();
-      p.registerComponentParser<Bus1PParser>();
       p.registerComponentParser<BusParser>();
-      p.registerComponentParser<Network1PParser>();
       p.registerComponentParser<NetworkParser>();
       p.registerComponentParser<TestComponentParser>();
-      p.registerComponentParser<ZipToGround1PParser>();
       p.registerComponentParser<ZipToGroundParser>();
    }
 }
