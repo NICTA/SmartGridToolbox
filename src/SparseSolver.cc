7c45f068266266d060f820910b45e92bd601f6c0
Dan Gordon <dan.gordon@nicta.com.au>
2013-08-21 13:42:45 +1000

    Fix a bug in SparseSolver.

--- src/SparseSolver.cc
+++ src/SparseSolver.cc
@@ -28,10 +28,18 @@ bool KLUSolve(const boost::numeric::ublas::compressed_matrix<double> & a,
    klu_numeric *Numeric;
    klu_common Common;
    int i;
+
    klu_defaults (&Common);
    Symbolic = klu_analyze (n, ap, ai, &Common);
    Numeric = klu_factor (ap, ai, ax, Symbolic, &Common);
-   bool ok = klu_solve(Symbolic, Numeric, 5, 1, b1, &Common) == 1;
+   bool ok = klu_solve(Symbolic, Numeric, n, 1, b1, &Common) == 1;
+
+   if (!ok)
+   {
+      std::cerr << "KLU failed." << std::endl;
+      std::cerr << "Status = " << Common.status << std::endl;
+   }
+
    klu_free_symbolic (&Symbolic, &Common);
    klu_free_numeric (&Numeric, &Common);
 
