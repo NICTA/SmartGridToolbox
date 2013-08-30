221c450588695019fdb1ffd26545d989fa8a16df
Dan Gordon <dan.gordon@nicta.com.au>
2013-08-30 15:02:19 +1000

    Working on units.

--- src/Units.h
+++ src/Units.h
@@ -10,167 +10,146 @@ namespace Units
 {
    constexpr const char * cdot = u8"\u00B7"; 
 
-   template<int m, int kg, int s, int A, int K>
+   template<int L, int M, int T, int I, int Th>
    class Dimensions
    {
       public:
-         static constexpr int mPow() {return L;}
-         static constexpr int kgPow() {return M;}
-         static constexpr int sPow() {return T;}
-         static constexpr int APow() {return I;}
-         static constexpr int KPow() {return Th;}
+         typedef Dimensions DimType;
+
+      public:
+         static constexpr int LDim() {return L;}
+         static constexpr int MDim() {return M;}
+         static constexpr int TDim() {return T;}
+         static constexpr int IDim() {return I;}
+         static constexpr int ThDim() {return Th;}
    };
 
-   template<typename T1, int m, int kg, int s, int A, int K>
-   class DimensionalQuantity : public Dimensions
+   using LDim = Dimensions<1, 0, 0, 0, 0>;
+   using MDim = Dimensions<0, 1, 0, 0, 0>;
+   using TDim = Dimensions<0, 0, 1, 0, 0>;
+   using IDim = Dimensions<0, 0, 0, 1, 0>;
+   using ThDim = Dimensions<0, 0, 0, 0, 0>;
+
+   template<typename D, typename V = double>
+   class DimensionalQuantity : public D 
    {
       public:
-         typedef T1 ValueType;
+         typedef V ValType;
 
       public:
-         static constexpr int mPow() {return m;}
-         static constexpr int kgPow() {return kg;}
-         static constexpr int sPow() {return s;}
-         static constexpr int APow() {return A;}
-         static constexpr int KPow() {return K;}
+         DimensionalQuantity(V stdVal) : stdVal_(stdVal) {}
 
-      public:
-         DimensionalQuantity(T1 stdVal) : stdVal_(stdVal) {}
-
-         const T1 & stdVal() const {return stdVal_;}
-         T1 & stdVal() {return stdVal_;}
-
-      public:
+         const V & stdVal() const {return stdVal_;}
+         V & stdVal() {return stdVal_;}
 
       private:
-         T1 stdVal_;
+         V stdVal_;
    };
 
-   template<typename T1>
-   class DimensionalQuantity<T1, 0, 0, 0, 0, 0>
+   template<typename V>
+   class DimensionalQuantity<Dimensions<0, 0, 0, 0, 0>, V>
    {
       public:
-         typedef T1 ValueType;
-      public:
-         static constexpr int mPow() {return 0;}
-         static constexpr int kgPow() {return 0;}
-         static constexpr int sPow() {return 0;}
-         static constexpr int APow() {return 0;}
-         static constexpr int KPow() {return 0;}
+         typedef V ValType;
 
       public:
-         operator const T1 &() const {return stdVal_;} 
-         operator T1 &() {return stdVal_;} 
+         DimensionalQuantity(V stdVal) : stdVal_(stdVal) {}
+
+         const V & stdVal() const {return stdVal_;}
+         V & stdVal() {return stdVal_;}
+
+         operator const V &() const {return stdVal_;} 
+         operator V &() {return stdVal_;} 
 
       private:
-         T1 stdVal_;
+         V stdVal_;
    };
 
-   template<typename T1, typename T2, int m, int kg, int s, int A, int K>
-   auto operator*(const DimensionalQuantity<T1, m, kg, s, A, K> & q, const T2 & scalar) 
-      -> DimensionalQuantity<decltype(q.stdVal() * scalar), m, kg, s, A, K>
+   template<typename D, typename V, typename V2>
+   auto operator*(const DimensionalQuantity<D, V> & q, const V2 & val) 
+      -> DimensionalQuantity<D, decltype(q.stdVal() * val)>
+   {
+      return {q.stdVal() * val};
+   };
+
+   template<typename D, typename V, typename V2>
+   auto operator*(const V2 & scalar, const DimensionalQuantity<D, V> & q)
+      -> DimensionalQuantity<D, decltype(q.stdVal() * scalar)>
    {
       return {q.stdVal() * scalar};
    };
 
-   template<typename T1, typename T2, int m, int kg, int s, int A, int K>
-   auto operator*(const T1 & scalar, const DimensionalQuantity<T2, m, kg, s, A, K> & q)
-      -> DimensionalQuantity<decltype(q.stdVal() * scalar), m, kg, s, A, K>
-   {
-      return {q.stdVal() * scalar};
-   };
-
-   template<typename T1, typename T2, int m, int kg, int s, int A, int K>
-   auto operator+(const DimensionalQuantity<T1, m, kg, s, A, K> & lhs,
-                  const DimensionalQuantity<T1, m, kg, s, A, K> & rhs)
-      -> DimensionalQuantity<decltype(lhs.stdVal() * rhs.stdVal()), m, kg, s, A, K>
+   template<typename D, typename V1, typename V2>
+   auto operator+(const DimensionalQuantity<D, V1> & lhs, const DimensionalQuantity<D, V2> & rhs)
+      -> DimensionalQuantity<D, decltype(lhs.stdVal() * rhs.stdVal())>
    {
       return {lhs.stdVal() + rhs.stdVal()};
    };
 
-   template<typename T1, typename T2, int m1, int m2, int kg1, int kg2, int s1, int s2, int A1, int A2,
-            int K1, int K2>
-   auto operator*(const DimensionalQuantity<T1, m1, kg1, s1, A1, K1> & lhs,
-                  const DimensionalQuantity<T2, m2, kg2, s2, A2, K2> & rhs) 
-      -> DimensionalQuantity<decltype(lhs.stdVal() * rhs.stdVal()), m1 + m2, kg1 + kg2, s1 + s2, A1 + A2, K1 + K2>
+   template<typename D1, typename D2, typename V1, typename V2>
+   auto operator*(const DimensionalQuantity<D1, V1> & lhs, const DimensionalQuantity<D2, V2> & rhs) 
+      -> DimensionalQuantity<Dimensions<D1::LDim() + D2::LDim(),
+                                        D1::MDim() + D2::MDim(),
+                                        D1::TDim() + D2::TDim(),
+                                        D1::IDim() + D2::IDim(),
+                                        D1::ThDim() + D2::ThDim()>,
+                             decltype(lhs.stdVal() * rhs.stdVal())>
    {
       return {lhs.stdVal() * rhs.stdVal()};
    }
 
-   template<typename T1, typename T2, int m1, int m2, int kg1, int kg2, int s1, int s2, int A1, int A2,
-            int K1, int K2>
-   auto operator/(const DimensionalQuantity<T1, m1, kg1, s1, A1, K1> & lhs,
-                  const DimensionalQuantity<T2, m2, kg2, s2, A2, K2> & rhs) 
-      -> DimensionalQuantity<decltype(lhs.stdVal() / rhs.stdVal()), m1 - m2, kg1 - kg2, s1 - s2, A1 - A2, K1 - K2>
+   template<typename V1, typename D1, typename V2, typename D2>
+   auto operator/(const DimensionalQuantity<D1, V1> & lhs, const DimensionalQuantity<D2, V2> & rhs) 
+      -> DimensionalQuantity<Dimensions<D1::LDim() - D2::LDim(),
+                                        D1::MDim() - D2::MDim(),
+                                        D1::TDim() - D2::TDim(),
+                                        D1::IDim() - D2::IDim(),
+                                        D1::ThDim() - D2::ThDim()>,
+                             decltype(lhs.stdVal() / rhs.stdVal())>
    {
       return {lhs.stdVal() / rhs.stdVal()};
    }
 
-   template<typename T1, int m, int kg, int s, int A, int K>
-   std::ostream & operator<<(std::ostream & os, const DimensionalQuantity<T1, m, kg, s, A, K> & q)
-   {
-      std::ostringstream ss;
-      ss << q.stdVal() << " ";
-      bool prevUnit = false;
-      if (m != 0) {ss << "m"; if (m != 1) ss << "^" << m; prevUnit = true;}
-      if (kg != 0) {if (prevUnit) ss << cdot << ss << "kg"; if (kg != 1) ss << "^" << kg; prevUnit = true;}
-      if (s != 0) {if (prevUnit) ss << cdot << "s"; if (s != 1) ss << "^" << s; prevUnit = true;}
-      if (A != 0) {if (prevUnit) ss << cdot << "A"; if (A != 1) ss << "^" << A; prevUnit = true;}
-      if (K != 0) {if (prevUnit) ss << cdot << "K"; if (K != 1) ss << "^" << K; prevUnit = true;}
-      return os << ss.str();
-   }
+   template<typename V = double> using Length = DimensionalQuantity<LDim, V>;
+   template<typename V = double> using Mass = DimensionalQuantity<MDim, V>;
+   template<typename V = double> using Time = DimensionalQuantity<TDim, V>;
+   template<typename V = double> using Current = DimensionalQuantity<IDim, V>;
+   template<typename V = double> using Temperature = DimensionalQuantity<ThDim, V>;
 
-   template<typename T1> using Length = DimensionalQuantity<T1, 1, 0, 0, 0, 0>;
-   template<typename T1> using Mass = DimensionalQuantity<T1, 0, 1, 0, 0, 0>;
-   template<typename T1> using Time = DimensionalQuantity<T1, 0, 0, 1, 0, 0>;
-   template<typename T1> using Current = DimensionalQuantity<T1, 0, 0, 0, 1, 0>;
-   template<typename T1> using Temperature = DimensionalQuantity<T1, 0, 0, 0, 0, 1>;
-
-   template<typename T1, int m, int kg, int s, int A, int K>
-   class Unit : public DimensionalQuantity<T1, m, kg, s, A, K>
+   template<typename D, typename V = double>
+   class Unit : public DimensionalQuantity<D, V>
    {
       public:
-         Unit(T1 & stdVal, const std::string & name) :
-            DimensionalQuantity<T1, m, kg, s, A, K>(stdVal), name_(name) 
-         {}
-         Unit(const DimensionalQuantity<T1, m, kg, s, A, K> & q, const std::string & name) :
-            DimensionalQuantity<T1, m, kg, s, A, K>(q),
-            name_(name) 
-         {}
-         const std::string & name() {return name_;}
+         Unit(V & stdVal, const std::string & name) : DimensionalQuantity<D, V>(stdVal), name_(name) {}
+         Unit(const DimensionalQuantity<D, V> & q, const std::string & name) :
+            DimensionalQuantity<D, V>(q), name_(name) {}
+
+         const std::string & name() const {return name_;}
 
       private:
          std::string name_;
    };
 
-   template<typename T1, typename T2, int m, int kg, int s, int A, int K>
+   extern const Unit<LDim> m = {1.0, "m"};
+   extern const Unit<MDim> kg = {1.0, "kg"};
+   extern const Unit<TDim> s = {1.0, "s"};
+   extern const Unit<IDim> A = {1.0, "A"};
+   extern const Unit<ThDim> K = {1.0, "K"};
+
+   template<typename D, typename V = double, typename V2 = double>
    class UnitQuantity
    {
       public:
-         UnitQuantity(const DimensionalQuantity<T1, m, kg, s, A, K> & q, const Unit<T2, m, kg, s, A, K> & u) :
-            q_(q), u_(u)
-         {}
+         UnitQuantity(const DimensionalQuantity<D, V> & q, const Unit<D, V2> & u) : q_(q), u_(u) {}
+
          friend std::ostream & operator<<(std::ostream & os, const UnitQuantity & uq)
          {
             return os << (uq.q_ / uq.u_) << " " << uq.u_.name();
          }
       private:
-         const DimensionalQuantity<T1, m, kg, s, A, K> & q_;
-         const Unit<T2, m, kg, s, A, K> & u_;
+         const DimensionalQuantity<D, V> & q_;
+         const Unit<D, V2> & u_;
    };
-
-   template<typename T1> using Length = DimensionalQuantity<T1, 1, 0, 0, 0, 0>;
-   template<typename T1> using Mass = DimensionalQuantity<T1, 0, 1, 0, 0, 0>;
-   template<typename T1> using Time = DimensionalQuantity<T1, 0, 0, 1, 0, 0>;
-   template<typename T1> using Current = DimensionalQuantity<T1, 0, 0, 0, 1, 0>;
-   template<typename T1> using Temperature = DimensionalQuantity<T1, 0, 0, 0, 0, 1>;
-
-   extern const Unit<double, 1, 0, 0, 0, 0> m = {1.0, "m"};
-   extern const Unit<double, 0, 1, 0, 0, 0> kg = {1.0, "kg"};
-   extern const Unit<double, 0, 0, 1, 0, 0> s = {1.0, "s"};
-   extern const Unit<double, 0, 0, 0, 1, 0> A = {1.0, "A"};
-   extern const Unit<double, 0, 0, 0, 0, 1> K = {1.0, "K"};
-   extern const Unit<double, 1, 0, -1, 0, 0> m_per_sec = {m.stdVal() / s.stdVal(), std::string("m") + cdot + "s^-1"};
 }
 
 #endif // UNITS_DOT_H
