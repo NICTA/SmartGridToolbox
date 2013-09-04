b820e4dfb29e06915ad560651e5bedd25934daf6
Dan Gordon <dan.gordon@nicta.com.au>
2013-09-04 16:25:54 +1000

    SolarPV code compiles.

--- src/SolarPV.h
+++ src/SolarPV.h
@@ -2,24 +2,48 @@
 #define SOLAR_PV_DOT_H
 
 #include "Component.h"
-#include "Weather.h"
+#include "DCPowerSourceBase.h"
+#include "Parser.h"
 
 namespace SmartGridToolbox
 {
+   class Weather;
+
+   class SolarPVParser : public ComponentParser
+   {
+      public:
+         static constexpr const char * componentName()
+         {
+            return "solar_PV";
+         }
+
+      public:
+         virtual void parse(const YAML::Node & nd, Model & mod) const override;
+
+         virtual void postParse(const YAML::Node & nd, Model & mod) const override;
+   };
+
    class SolarPV : public DCPowerSourceBase
    {
       /// @name My public member functions. 
       /// @{
       public:
-         DCPowerSourceBase(const std::string & name) : Component(name), weather(nullptr), efficiency_(1.0) {}
+         SolarPV(const std::string & name) :
+            DCPowerSourceBase(name), weather_(nullptr), efficiency_(1.0), plane_({0.0, 1.0, 0.0})
+         {}
 
          virtual double PDC() const override;
 
          void setWeather(const Weather & weather) {weather_ = &weather;}
 
+         void setEfficiency(double efficiency) {efficiency_ = efficiency;}
+
+         void setPlane(const Array<double, 3> & plane) {plane_ = plane;}
+
       public:
          const Weather * weather_;
          double efficiency_;
+         Array<double, 3> plane_; // TODO : more than one plane?
    };
 }
 
