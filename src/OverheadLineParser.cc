09b9d6e5c4739275b451efc42ae8d53dbba69fcf
Dan Gordon <dan.gordon@nicta.com.au>
2014-01-13 16:22:59 +1100

    Add OverheadLineParser (incomplete).

---
+++ src/OverheadLineParser.cc
@@ -0,0 +1,37 @@
+#include "OverheadLineParser.h"
+
+namespace SmartGridToolbox
+{
+   void OverheadLineParser::parse(const YAML::Node & nd, Model & mod, const ParserState & state) const
+   {
+      SGT_DEBUG(debug() << "OverheadLine : parse." << std::endl);
+
+      assertFieldPresent(nd, "name");
+      assertFieldPresent(nd, "network");
+      assertFieldPresent(nd, "bus_0");
+      assertFieldPresent(nd, "bus_1");
+      assertFieldPresent(nd, "phases_0");
+      assertFieldPresent(nd, "phases_1");
+      assertFieldPresent(nd, "length");
+      assertFieldPresent(nd, "line_resistivity");
+      assertFieldPresent(nd, "earth_resistivity");
+      assertFieldPresent(nd, "distance_matrix");
+
+      string name = state.expandName(nd["name"].as<std::string>());
+      Phases phases0 = nd["phases_0"].as<Phases>();
+      Phases phases1 = nd["phases_1"].as<Phases>();
+      double length = nd["length"].as<double>();
+      int nNeutral = nd["n_neutral"].as<int>();
+      ublas::vector<double> lineResistivity = nd["line_resistivity"].as<ublas::vector<double>>();
+      double earthResistivity = nd["earth_resistivity"].as<double>();
+      ublas::matrix<double> distMatrix = nd["distance_matrix"].as<ublas::matrix<double>>();
+      
+      OverheadLine & comp = mod.newComponent<OverheadLine>(name, phases0, phases1, length, nNeutral, lineResistivity,
+                                                           earthResistivity, distMatrix, 50.0);
+   }
+
+   void OverheadLineParser::postParse(const YAML::Node & nd, Model & mod, const ParserState & state) const
+   {
+
+   }
+}
---
+++ src/OverheadLineParser.h
@@ -0,0 +1,22 @@
+#ifndef OVERHEAD_LINE_PARSER_DOT_H
+#define OVERHEAD_LINE_PARSER_DOT_H
+
+#include <SmartGridToolbox/Parser.h>
+
+namespace SmartGridToolbox
+{
+   class OverheadLineParser : public ParserPlugin
+   {
+      public:
+         static constexpr const char * pluginKey()
+         {
+            return "overhead_line";
+         }
+
+      public:
+         virtual void parse(const YAML::Node & nd, Model & mod, const ParserState & state) const override;
+         //virtual void postParse(const YAML::Node & nd, Model & mod, const ParserState & state) const override;
+   };
+}
+
+#endif // OVERHEAD_LINE_PARSER_DOT_H
