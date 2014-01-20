54c2a8c24643a0b697747cc6610a008259b854fa
Dan Gordon <dan.gordon@nicta.com.au>
2014-01-20 17:07:34 +1100

    Working on single phase transformer.

--- src/SinglePhaseTransformer.cc
+++ src/SinglePhaseTransformer.cc
@@ -2,5 +2,24 @@
 
 namespace SmartGridToolbox
 {
+   SinglePhaseTransformer::SinglePhaseTransformer(const std::string & name, Phase phase0, Phase phase1, &
+                                                  Complex alpha, Complex ZLeak)
+      : Branch(name, phase0, phase1),
+        alpha_(alpha),
+        ZLeak_(ZLeak)
 
+   {
+      recalcY();      
+   }
+
+   void SinglePhaseTransformer::recalcY()
+   {
+      double a2 = real(a * conj(a));
+      ublas::matrix<Complex> YNode(2, 2, czero);
+      YNode(0, 0) = YLeak_ / a2;  
+      YNode(0, 1) = -YLeak_ / conj(a);  
+      YNode(1, 0) = -YLeak_ / a;  
+      YNode(1, 1) = YLeak_;
+      setY(YNode);
+   }
 };
--- src/SinglePhaseTransformer.h
+++ src/SinglePhaseTransformer.h
@@ -8,8 +8,11 @@ namespace SmartGridToolbox
    class SinglePhaseTransformer : public Branch
    {
       public:
-         SinglePhaseTransformer(const std::string & name, Complex alpha, Complex ZLeak);
+         SinglePhaseTransformer(const std::string & name, Phase phase0, Phase phase1, Complex alpha, Complex ZLeak);
       
+      private:
+         void recalcY();
+
       private:
          Complex alpha01; ///< Complex turns ratio, n0/n1.
          Complex ZLeak;   ///< Leakage impedance.
