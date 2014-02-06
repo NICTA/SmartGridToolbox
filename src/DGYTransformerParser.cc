e8e2a4b9c1350472a9a9a7b66c2ef72382c4c8a5
Dan Gordon <dan.gordon@nicta.com.au>
2014-02-06 15:58:35 +1100

    Parser for DGYTransformer.

---
+++ src/DGYTransformerParser.cc
@@ -0,0 +1,72 @@
+#include "DGYTransformerParser.h"
+
+#include "Network.h"
+#include "DGYTransformer.h"
+
+namespace SmartGridToolbox
+{
+   void DGYTransformerParser::parse(const YAML::Node & nd, Model & mod, const ParserState & state) const
+   {
+      SGT_DEBUG(debug() << "DGYTransformer : parse." << std::endl);
+
+      assertFieldPresent(nd, "name");
+      assertFieldPresent(nd, "network");
+      assertFieldPresent(nd, "bus_0");
+      assertFieldPresent(nd, "bus_1");
+      assertFieldPresent(nd, "complex_turns_ratio_01");
+      assertFieldPresent(nd, "leakage_impedance");
+
+      string name = state.expandName(nd["name"].as<std::string>());
+      Complex alpha = nd["complex_turns_ratio_01"].as<Complex>();
+      Complex ZLeak = nd["leakage_impedance"].as<Complex>();
+      
+      mod.newComponent<DGYTransformer>(name, alpha, ZLeak);
+   }
+
+   void DGYTransformerParser::postParse(const YAML::Node & nd, Model & mod, const ParserState & state) const
+   {
+      SGT_DEBUG(debug() << "DGYTransformer : postParse." << std::endl);
+
+      string name = state.expandName(nd["name"].as<std::string>());
+      DGYTransformer* comp = mod.component<DGYTransformer>(name);
+
+      const std::string networkStr = state.expandName(nd["network"].as<std::string>());
+      Network* networkComp = mod.component<Network>(networkStr);
+      if (networkComp != nullptr)
+      {
+         networkComp->addBranch(*comp);
+      }
+      else
+      {
+         error() << "For component " << name <<  ", network " << networkStr 
+                 <<  " was not found in the model." << std::endl;
+         abort();
+      }
+
+      const std::string bus0Str = state.expandName(nd["bus_0"].as<std::string>());
+      Bus* bus0Comp = mod.component<Bus>(bus0Str);
+      if (networkComp != nullptr)
+      {
+         comp->setBus0(*bus0Comp);
+      }
+      else
+      {
+         error() << "For component " << name <<  ", bus " << bus0Str 
+                 <<  " was not found in the model." << std::endl;
+         abort();
+      }
+
+      const std::string bus1Str = state.expandName(nd["bus_1"].as<std::string>());
+      Bus* bus1Comp = mod.component<Bus>(bus1Str);
+      if (networkComp != nullptr)
+      {
+         comp->setBus1(*bus1Comp);
+      }
+      else
+      {
+         error() << "For component " << name <<  ", bus " << bus1Str 
+                 <<  " was not found in the model." << std::endl;
+         abort();
+      }
+   }
+}
---
+++ src/DGYTransformerParser.h
@@ -0,0 +1,22 @@
+#ifndef DGY_TRANSFORMER_PARSER
+#define DGY_TRANSFORMER_PARSER
+
+#include <SmartGridToolbox/Parser.h>
+
+namespace SmartGridToolbox
+{
+   class DGYTransformerParser : public ParserPlugin
+   {
+      public:
+         static constexpr const char* pluginKey()
+         {
+            return "delta_g_wye_transformer";
+         }
+
+      public:
+         virtual void parse(const YAML::Node & nd, Model & mod, const ParserState & state) const override;
+         virtual void postParse(const YAML::Node & nd, Model & mod, const ParserState & state) const override;
+   };
+}
+
+#endif // DGY_TRANSFORMER_PARSER
