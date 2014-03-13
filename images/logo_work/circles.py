aca6f719717014810a09b26b796e73a521494a71
Dan Gordon <dan.gordon@nicta.com.au>
2014-03-14 10:26:15 +1100

    More logo work.

--- images/logo_work/circles.py
+++ images/logo_work/circles.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ncirc = 40 
-nperiods = 1 
-min_rad = 0.002
-max_rad = 0.01
+ncirc = 20 
+nperiods = 0.5 
+min_rad = 0.003
+max_rad = 0.015
 rad_mult = max_rad - min_rad
-height_mult = 0.03
+height_mult = 0.02
+
+nicta_green='#5da733'
+nicta_purple='#8d65d2'
 
 plt.axis('off')
 ax = plt.gcf().gca()
@@ -14,13 +17,15 @@ for i in range(0, ncirc + 1):
     x = float(i)/ncirc
     circ = plt.Circle([x, height_mult*(np.sin(x*2*nperiods*np.pi)) + 0.5],
                       rad_mult*(np.sin(x*2*nperiods*np.pi)**2) + min_rad,
-                      color='#8d65d2',
+                      color=nicta_green,
                       clip_on = False)
     ax.add_artist(circ)
     circ = plt.Circle([x, -height_mult*(np.sin(x*2*nperiods*np.pi)) + 0.5],
                       rad_mult*(np.sin(x*2*nperiods*np.pi)**2) + min_rad,
-                      color='#5da733',
+                      color=nicta_purple,
                       clip_on=False)
     ax.add_artist(circ)
+    
     ax.set_aspect(1.0)
-    plt.savefig('circles.pdf')
+
+plt.savefig('circles.pdf')
