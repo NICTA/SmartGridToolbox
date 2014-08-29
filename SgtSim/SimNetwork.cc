f73dbb45fe5dcba4667f74a4ee7afda0dfe4bfaa
Dan Gordon <dan.gordon@nicta.com.au>
2014-08-29 15:20:49 +1000

    WIP.

--- SgtSim/SimNetwork.cc
+++ SgtSim/SimNetwork.cc
@@ -10,6 +10,55 @@
 
 namespace SmartGridToolbox
 {
+   void SimNetwork::addNode(std::shared_ptr<Bus> bus)
+   {
+      Network::addNode(bus);
+
+      dependsOn(bus);
+
+      bus->controlChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      bus->isInServiceChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+   }
+
+   void SimNetwork::addArc(std::shared_ptr<Branch> branch, const std::string& bus0Id,
+         const std::string& bus1Id)
+   {
+      Network::addArc(branch, bus0Id, bus1Id);
+
+      branch->admittanceChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      branch->isInServiceChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+   }
+
+   void SimNetwork::addGen(std::shared_ptr<Gen> gen, const std::string& busId)
+   {
+      Network::addGen(gen, busId);
+
+      gen->setpointChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      gen->generationChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      gen->isInServiceChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+   }
+
+   void SimNetwork::addZip(std::shared_ptr<Zip> zip, const std::string& busId)
+   {
+      Network::addZip(zip, busId);
+
+      dependsOn(zip);
+
+      zip->injectionChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      zip->setpointChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+      zip->isInServiceChanged().addAction([this](){needsUpdate().trigger();}, 
+            "Trigger Network " + id() + " needs update");
+   }
+
    void SimNetwork::updateState(Time t)
    {
       SGT_DEBUG(debug() << "SimNetwork : update state." << std::endl);
--- SgtSim/SimNetwork.h
+++ SgtSim/SimNetwork.h
@@ -10,33 +10,37 @@ namespace SmartGridToolbox
    /// @ingroup PowerFlowCore
    class SimNetwork : public Network, public SimComponent
    {
+      public:
+
+      /// @name Lifecycle.
+      /// @{
+         
+         SimNetwork(const std::string& id, double PBase) : Network(id, PBase) {}
+
+      /// @}
+      
       /// @name Overridden member functions from Network.
       /// @{
       
       public:
 
-         virtual void addBus(std::shared_ptr<Bus> bus) override;
-         virtual void addBranch(std::shared_ptr<Branch> branch) override;
-         virtual void addGen(std::shared_ptr<Gen> gen) override;
-         virtual void addZip(std::shared_ptr<Zip> zip) override;
+         virtual void addNode(std::shared_ptr<Bus> bus) override;
+         virtual void addArc(std::shared_ptr<Branch> branch, const std::string& bus0Id,
+                             const std::string& bus1Id) override;
+         virtual void addGen(std::shared_ptr<Gen> gen, const std::string& busId) override;
+         virtual void addZip(std::shared_ptr<Zip> zip, const std::string& busId) override;
 
       /// @name Overridden member functions from SimComponent.
       /// @{
       
       public:
+
          // virtual Time validUntil() const override;
 
       protected:
+
          // virtual void initializeState() override;
          virtual void updateState(Time t) override;
-
-      /// @}
-
-      public:
-      /// @name My public member functions.
-      /// @{
-         
-         SimNetwork(const std::string& id, double PBase) : Network(id, PBase) {}
 
       /// @}
    };
