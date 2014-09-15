185284e533f7fbe7a8a67e4bd52c650fd74732e0
Dan Gordon <dan.gordon@nicta.com.au>
2014-09-15 12:12:42 +1000

    Tweak run_config.

--- src/run_config.cc
+++ src/run_config.cc
@@ -16,5 +16,12 @@ int main(int argc, const char** argv)
    SmartGridToolbox::Parser<Simulation> p;
    p.parse(configName, sim);
    sim.initialize();
-   while (sim.doNextUpdate());
+   bool ok = true;
+   while (ok)
+   {
+      Log().message() << "Doing timestep at " << sim.currentTime() << std::endl;
+      Indent _;
+      ok = sim.doTimestep();
+      Log().message() << "Now at " << sim.currentTime() << std::endl;
+   }
 }
