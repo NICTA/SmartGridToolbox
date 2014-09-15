6c3720e08f10a0a5953e4f0ea546a15d9734b646
Dan Gordon <dan.gordon@nicta.com.au>
2014-09-15 16:08:57 +1000

    Event output.

--- SgtCore/Event.cc
+++ SgtCore/Event.cc
@@ -6,10 +6,10 @@ namespace SmartGridToolbox
 {
    void Event::trigger()
    {
-      SGT_DEBUG(Log().debug() << "Event was triggered: " << description_ << std::endl);
+      SGT_DEBUG(Log().debug() << "Event was triggered: " << description_ << std::endl; Indent _);
       for (const Action& action : actions_)
       {
-         SGT_DEBUG(Log().debug() << "Event perform action: " << action.description() << std::endl);
+         SGT_DEBUG(Log().debug() << "Event perform action: " << action.description() << std::endl; Indent _);
          action.perform();
          SGT_DEBUG(Log().debug() << "Event performed action: " << action.description() << std::endl);
       }
