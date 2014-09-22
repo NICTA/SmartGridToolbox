82f5334b6c92746e1eb02f4b06c48ef803ab8c7d
Dan Gordon <dan.gordon@nicta.com.au>
2014-09-22 23:56:39 +1000

    Working on cloud cover.

--- src/generate_cloud.m
+++ src/generate_cloud.m
@@ -1,6 +1,6 @@
 tvec = 0:5:24*60*7;
 Tvec = 60:120:60*24;
-xvec = rand(size(Tvec));
+xvec = rand(size(Tvec)).*exp(-((Tvec-18*60)/(12*60)).^2);
 pvec = rand(size(Tvec))*2*pi;
 cvec = zeros(size(tvec));
 for i = 1:length(Tvec)
@@ -8,9 +8,10 @@ for i = 1:length(Tvec)
 end
 cvec -= min(cvec);
 cvec /= max(cvec);
+cvec = 1-(1-cvec).^4;
 
 dat = [tvec', cvec'];
 
 fp = fopen('cloud_cover.txt', 'w');
-fprintf(fp, '%d, %5.3f\n', dat');
+fprintf(fp, '%d %5.3f\n', dat');
 fclose(fp);
