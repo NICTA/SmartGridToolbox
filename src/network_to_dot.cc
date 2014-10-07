7e0ff24ea2cabeb5fa4bb53715f37c97823956cd
Dan Gordon <dan.gordon@nicta.com.au>
2014-10-07 20:05:18 +1100

    Working on residential_a example.

--- src/network_to_dot.cc
+++ src/network_to_dot.cc
@@ -8,13 +8,15 @@ using namespace SmartGridToolbox;
 
 int main(int argc, const char** argv)
 {
-   if (argc != 3)
+   if (argc < 3)
    {
-      Log().fatal() << "Usage: " << argv[0] << " config_name output_name" << std::endl;
+      Log().fatal() << "Usage: " << argv[0] << " config_name output_name [option ...]" << std::endl;
    }
 
    const char* configName = argv[1];
    const char* outName = argv[2];
+   int nOpt = argc - 3;
+   const char** opts = argv + 3;
 
    std::ofstream out(outName);
 
@@ -27,14 +29,19 @@ int main(int argc, const char** argv)
 
    out << "graph G {" << std::endl;
    {
-      StreamIndent _(out); 
+      StreamIndent _(out);
+      for (int i = 0; i < nOpt; ++i)
+      {
+         out << opts[i] << std::endl;
+      }
       for (auto& nd : netw->nodes())
       {
          out << nd->bus()->id() << std::endl;
       }
       for (auto& arc : netw->arcs())
       {
-         out << arc->node0()->bus()->id() << " -- " << arc->node1()->bus()->id() << std::endl;
+         out << arc->node0()->bus()->id() << " -- " << arc->node1()->bus()->id() 
+            << " [label=" << arc->branch()->id() << "]" << std::endl;
       }
    }
    out << "}" << std::endl;
