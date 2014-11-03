d550a9bb6be50b46b728e0e014b916ff85eff28c
Dan Gordon <dan.gordon@nicta.com.au>
2014-11-04 10:30:57 +1100

    WIP on Property.

--- SgtCore/Property.h
+++ SgtCore/Property.h
@@ -2,6 +2,7 @@
 #define PROPERTY_DOT_H
 
 #include<map>
+#include<vector>
 #include<string>
 
 namespace SmartGridToolbox
@@ -12,7 +13,15 @@ namespace SmartGridToolbox
          virtual ~PropertyBase() = default;
    };
 
-   template<typename T> class Property : public PropertyBase
+   template<class T> using Getter = const T& ();
+   template<class T> using Setter = void (const T&);
+
+   template<class T> struct NoGetter {};
+   template<class T> struct NoSetter {};
+
+   template<typename T, template<typename> class G = NoGetter, template<typename> class S = NoSetter> class Property;
+
+   template<typename T> class Property<T, NoGetter, NoSetter> : public PropertyBase
    {
       public:
 
@@ -21,38 +30,59 @@ namespace SmartGridToolbox
             // Empty.
          }
 
-         const T& val() const {return val_;}
-         T& val() {return val_;}
+         const T& get() const {return val_;}
 
-         operator T() {return val_;}
+         void set(const T& val) const {val_ = val;}
 
       private:
 
          T val_;
    };
 
-   template<typename T> class Property<T ()> : public PropertyBase
+   template<typename T, template<typename> class G> class Property<T, G, NoSetter> : public PropertyBase
    {
       public:
 
-         template<typename... Args> Property(Args&&... args) : func_(std::forward<Args>(args)...)
+         template<typename Arg> Property(Arg&& arg) : get_(std::forward<Arg>(arg))
          {
             // Empty.
          }
 
-         const T val() const {return func_();}
-         T val() {return func_();}
-
-         operator T() {return func_();}
+         const T& get() const {return get_();}
 
       private:
 
-         std::function<T ()> func_;
+         std::function<Getter<T>> get_;
+   };
+
+   template<typename T, template<typename> class G, template<typename> class S> class Property : public Property<T, G>
+   {
+      public:
+
+         template<typename GetterArg, typename SetterArg> Property(GetterArg&& getArg, SetterArg&& setArg) :
+            Property<T, G>(get_(std::forward<GetterArg>(getArg))),
+            set_(std::forward<SetterArg>(setArg))
+         {
+            // Empty.
+         }
+
+         
+         void set(const T& val)
+         {
+            set_(val);
+         }
+
+      private:
+
+         std::function<Setter<T>> set_;
    };
 
    class Properties
    {
       public:
+
+         typedef std::map<std::string, PropertyBase*> MapType;
+         typedef MapType::const_iterator ConstIteratorType;
 
          ~Properties()
          {
@@ -71,6 +101,16 @@ namespace SmartGridToolbox
          {
             auto it = properties_.find(key);
             return it == properties_.end() ? nullptr : dynamic_cast<Property<T>*>(it->second);
+         }
+
+         ConstIteratorType begin()
+         {
+            return properties_.cbegin();
+         }
+
+         ConstIteratorType end()
+         {
+            return properties_.cend();
          }
 
       private:
