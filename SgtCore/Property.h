a675fb9dbe0e7e016b5ff86b777046ae7e83cb14
Dan Gordon <dan.gordon@nicta.com.au>
2014-11-07 10:21:54 +1100

    WIP.

--- SgtCore/Property.h
+++ SgtCore/Property.h
@@ -1,7 +1,5 @@
-#ifndef PROPERTY_DOT_H
-#define PROPERTY_DOT_H
-
-#include <SgtCore/Common.h>
+#ifndef PROPERTY_DOTH
+#define PROPERTY_DOTH
 
 #include<map>
 #include<stdexcept>
@@ -10,11 +8,6 @@
 
 namespace SmartGridToolbox
 {
-   struct PropertyNone {};
-   typedef PropertyNone NoTarget;
-   typedef PropertyNone NoGetter;
-   typedef PropertyNone NoSetter;
-
    class PropertyBase
    {
 
@@ -23,28 +16,24 @@ namespace SmartGridToolbox
          virtual ~PropertyBase() = default;
    };
 
-   template<typename TargetType_, typename GetterReturnType_> class Gettable
+   template<class T> using ByValue = T;
+   template<class T> using ByConstRef = const T&;
+
+   template<typename T, template<typename> class RetByType> class Gettable
    {
       public:
 
-         virtual GetterReturnType_ get(const TargetType_& target) const = 0;
+         virtual RetByType<T> get() const = 0;
    };
 
-   template<typename TargetType_, typename SetterArgType_> class Settable
-   {
-      public:
+   struct PropertyNone {};
+   typedef PropertyNone NoTarget;
+   typedef PropertyNone NoGetter;
 
-         virtual void set(TargetType_& target, SetterArgType_ val) = 0;
-   };
+   template<typename T, template<typename> class RetByType, typename TargType = NoTarget> class Property;
 
-   template<typename BaseType_, typename TargetType_ = NoTarget, typename GetterReturnType_ = NoGetter, 
-      typename SetterArgType_ = NoSetter> class Property;
-
-   template<typename BaseType_>
-      class Property<BaseType_, NoTarget, NoGetter, NoSetter> :
-      public PropertyBase,
-      public Gettable<NoTarget, const BaseType_&>,
-      public Settable<NoTarget, const BaseType_&>
+   template<typename T, template<typename> class RetByType>
+      class Property<T, RetByType, NoGetter> : public PropertyBase, public Gettable<T, RetByType>
    {
       public:
 
@@ -53,130 +42,30 @@ namespace SmartGridToolbox
             // Empty.
          }
 
-         virtual const BaseType_& get() const override {return val_;}
-
-         virtual void set(const BaseType_& val) override {val_ = val;}
+         virtual RetByType<T> get() const override {return val_;}
 
       private:
 
-         BaseType_ val_;
+         T val_;
    };
 
-   template<typename BaseType_, typename TargetType_, typename GetterReturnType_>
-      class Property<BaseType_, TargetType_, GetterReturnType_, NoSetter> :
-      public PropertyBase, public Gettable<TargetType_, const BaseType_&>
+   template<typename T, template<typename> class RetByType, typename TargType>
+      class Property : public PropertyBase, public Gettable<T, RetByType>
    {
       public:
 
-         template<typename Arg> Property(Arg&& arg) : get_(std::forward<Arg>(arg))
+         template<typename Arg> Property(const TargType& targ, Arg&& arg) : targ_(targ), get_(std::forward<Arg>(arg))
          {
             // Empty.
          }
 
-         virtual GetterReturnType_ get(const TargetType_& target) const {return get_(target);}
+         virtual RetByType<T> get() const override {return get_(targ_);}
 
       private:
 
-         std::function<GetterReturnType_ (const TargetType_&)> get_;
-   };
-
-   template<typename BaseType_, typename TargetType_, typename GetterReturnType_, typename SetterArgType_>
-      class Property :
-      public Property<BaseType_, TargetType_, GetterReturnType_>, public Settable<TargetType_, SetterArgType_>
-   {
-      public:
-
-         template<typename GetterArg, typename SetterArg> Property(GetterArg&& getArg, SetterArg&& setArg) :
-            Property<BaseType_, TargetType_, GetterReturnType_>(std::forward<GetterArg>(getArg)),
-            set_(std::forward<SetterArg>(setArg))
-         {
-            // Empty.
-         }
-         
-         virtual void set(TargetType_& target, SetterArgType_ val)
-         {
-            set_(target, val);
-         }
-
-      private:
-
-         std::function<void (TargetType_&, SetterArgType_)> set_;
-   };
-
-   class HasProperties
-   {
-      private:
-
-         typedef std::map<std::string, PropertyBase*> MapType;
-
-      public:
-
-         typedef MapType::const_iterator ConstIteratorType;
-         typedef MapType::iterator IteratorType;
-
-      public:
-
-         ~HasProperties()
-         {
-            for (auto pair : properties_)
-            {
-               delete pair.second;
-            }
-         }
-
-         template<typename BaseType_, typename TargetType_ = NoTarget, typename GetterReturnType_ = NoGetter,
-            typename SetterArgType_ = NoSetter, typename... Args> void add(const std::string& key, Args&&... args)
-         {
-            properties_[key] = new Property<BaseType_, TargetType_, GetterReturnType_, SetterArgType_>(
-                  std::forward<Args>(args)...);
-         }
-
-         template<typename TargetType_, typename GetterReturnType_>
-            GetterReturnType_ get(const std::string& key) const
-         {
-            auto p = dynamic_cast<const Gettable<TargetType_, GetterReturnType_>*>(properties_.at(key));
-            if (p == nullptr)
-            {
-               throw std::out_of_range(std::string("Property ") + key + " not found.");
-            }
-            return p->get(dynamic_cast<const TargetType_&>(*this));
-         }
-         
-         template<typename TargetType_, typename SetterArgType_>
-            void set(const std::string& key, const SetterArgType_& val)
-         {
-            auto p = dynamic_cast<const Settable<TargetType_, SetterArgType_>*>(properties_.at(key));
-            if (p == nullptr)
-            {
-               throw std::out_of_range(std::string("Property ") + key + " not found.");
-            }
-            p->set(dynamic_cast<const TargetType_&>(*this), val);
-         }
-
-         ConstIteratorType cbegin() const
-         {
-            return properties_.cbegin();
-         }
-
-         ConstIteratorType cend() const
-         {
-            return properties_.cend();
-         }
-
-         IteratorType begin()
-         {
-            return properties_.begin();
-         }
-
-         IteratorType end()
-         {
-            return properties_.end();
-         }
-
-      private:
-
-         std::map<std::string, PropertyBase*> properties_;
+         const TargType& targ_;
+         std::function<RetByType<T> (const TargType&)> get_;
    };
 }
 
-#endif // PROPERTY_DOT_H
+#endif // PROPERTY_DOTH
