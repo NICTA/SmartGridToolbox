eec8be04ff883c2f40014e776a1549a297ab309f
Dan Gordon <dan.gordon@nicta.com.au>
2014-12-02 02:58:18 +1100

    ResidentialA example is working better now.

--- examples/ResidentialA/generate_loads.m
+++ examples/ResidentialA/generate_loads.m
@@ -1,24 +1,27 @@
+mins_in_day = 24 * 60;
+
+n_days = 7;
 dt = 15;
-n_in_day = 24 * 60 / dt;
-ndays_in_dataset = 28;
+dt_b = 5;
+
+n_in_dataset = n_days * mins_in_day / dt;
 
 dat = load('loads.txt');
 dat = dat(:, randperm(6)); % To randomize house sizes.
-dat /= 1000; % From W to kW.
+dat /= 1e6; % From W to MW.
 
-% n_data_sets = floor(6 * 365 / ndays_in_dataset);
 n_data_sets = 50;
 
-t_vec = (0:15:24*60)'; % Note keep the next point for interpolation.
-t_vec_b = (0:5:24*60-5)';
+t_vec = (0:dt:n_days*mins_in_day)'; % Note keep the next point for interpolation.
+t_vec_b = (0:dt_b:n_days*mins_in_day-dt_b)';
 z_vec = zeros(size(t_vec_b));
 
 for i = 1:n_data_sets
-   irow = 1 + 96 * floor((i - 1) / 6);
+   irow = 1 + n_in_dataset * floor((i - 1) / 6);
    icol = mod(i - 1, 6) + 1;
-   ld_i = dat(irow:irow+96, icol); % Keep the next point for interpolation.
-   ld_i_b = interp1(t_vec, ld_i, t_vec_b, 'spline');
-   dat_i = [t_vec_b z_vec, z_vec, ld_i_b];
+   ld_i = dat(irow:irow+n_in_dataset, icol); % Keep the next point for interpolation.
+   ld_i_b = -interp1(t_vec, ld_i, t_vec_b, 'spline');
+   dat_i = [t_vec_b, z_vec, z_vec, ld_i_b];
    fname = ['loads/load_', num2str(i-1), '.txt'];
    fp = fopen(fname, 'w+');
    fprintf(fp, '%d %d %d %f\n', dat_i');
