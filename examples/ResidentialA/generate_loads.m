9e3ff7d5f18e56ebd409b2665aa5f9f3469f1d3f
Dan Gordon <dan.gordon@nicta.com.au>
2014-12-02 15:52:10 +1100

    Final (?) mods to residential_a.

--- examples/ResidentialA/generate_loads.m
+++ examples/ResidentialA/generate_loads.m
@@ -7,6 +7,7 @@ dt_b = 5;
 n_in_dataset = n_days * mins_in_day / dt;
 
 dat = load('loads.txt');
+rand('state', 0);
 dat = dat(:, randperm(6)); % To randomize house sizes.
 dat /= 1e6; % From W to MW.
 
@@ -20,7 +21,7 @@ for i = 1:n_data_sets
    irow = 1 + n_in_dataset * floor((i - 1) / 6);
    icol = mod(i - 1, 6) + 1;
    ld_i = dat(irow:irow+n_in_dataset, icol); % Keep the next point for interpolation.
-   ld_i_b = -interp1(t_vec, ld_i, t_vec_b, 'spline');
+   ld_i_b = -0.5 * interp1(t_vec, ld_i, t_vec_b, 'spline');
    dat_i = [t_vec_b, z_vec, z_vec, ld_i_b];
    fname = ['loads/load_', num2str(i-1), '.txt'];
    fp = fopen(fname, 'w+');
