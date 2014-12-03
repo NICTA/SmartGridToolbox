10f0e675b71079839635c64adc676f2cdb669d0c
Dan Gordon <dan.gordon@nicta.com.au>
2014-12-03 12:09:17 +1100

    Tweak output of solve_network.

--- src/solve_network.cc
+++ src/solve_network.cc
@@ -13,14 +13,18 @@ int main(int argc, char** argv)
    SmartGridToolbox::Parser<Network> p;
    p.parse(fname, nw);
    auto print = [&](){
+      printf("--------------------------------------------------------------------------\n");
+      printf("%12s : %9s %9s %9s %9s %9s %9s\n",
+            "bus", "|V|", "theta", "P_gen", "Q_gen", "P_load", "Q_load");
+      printf("--------------------------------------------------------------------------\n");
       for (auto nd : nw.nodes())
       {
          auto bus = nd->bus();
          auto V = nw.V2Pu(bus->V()[0], bus->VBase());
          auto SGen = nd->SGen()[0];
-         auto SZip = nd->SZip()[0];
+         auto SLoad = -nd->SZip()[0];
          printf("%12s : %9.4f %9.4f %9.4f %9.4f %9.4f %9.4f\n", bus->id().c_str(), std::abs(V), std::arg(V)*180.0/pi,
-               SGen.real(), SGen.imag(), SZip.real(), SZip.imag());
+               SGen.real(), SGen.imag(), SLoad.real(), SLoad.imag());
       }
    };
    print();
