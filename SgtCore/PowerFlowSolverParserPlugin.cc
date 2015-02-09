67ca9e1157b28ed6d31b71421b47fec61fdb8e95
Dan Gordon <dan.gordon@nicta.com.au>
2015-02-09 12:09:55 +1100

    Add files.

---
+++ SgtCore/PowerFlowSolverParserPlugin.cc
@@ -0,0 +1,48 @@
+#include "../config.h"
+
+#include "PowerFlowSolverParserPlugin.h"
+
+#include "Network.h"
+#include "PowerFlowNrSolver.h"
+#ifdef ENABLE_POWER_TOOLS 
+#include "PowerFlowPtSolver.h"
+#endif
+#ifdef ENABLE_POWER_TOOLS_PP
+#include "PowerFlowPtPpSolver.h"
+#endif
+#include "YamlSupport.h"
+
+namespace SmartGridToolbox
+{
+   void PowerFlowSolverParserPlugin::parse(const YAML::Node& nd, Network& netw, const ParserBase& parser) const
+   {
+      auto key = nd.as<std::string>();
+      if (key == "nr")
+      {
+         Log().message() << "Using Newton-Raphson solver." << std::endl;
+         netw.setSolver(std::unique_ptr<PowerFlowNrSolver>(new PowerFlowNrSolver));
+      }
+      else if (key == "opf_pt")
+      {
+#ifdef ENABLE_POWER_TOOLS 
+         Log().message() << "Using OPF (PowerTools) solver." << std::endl;
+         netw.setSolver(std::unique_ptr<PowerFlowPtSolver>(new PowerFlowPtSolver));
+#else // ENABLE_POWER_TOOLS 
+         Log().fatal() 
+            << "OPF solver is not available, since SmartGridToolbox was not compiled with --enable-power-tools."
+            << std::endl;
+#endif // ENABLE_POWER_TOOLS
+      }
+      else if (key == "opf_pt_pp")
+      {
+#ifdef ENABLE_POWER_TOOLS_PP
+         Log().message() << "Using OPF (PowerTools++) solver." << std::endl;
+         netw.setSolver(std::unique_ptr<PowerFlowPtPpSolver>(new PowerFlowPtPpSolver));
+#else // ENABLE_POWER_TOOLS_PP
+         Log().fatal() 
+            << "OPF solver is not available, since SmartGridToolbox was not compiled with --enable-power-tools++."
+            << std::endl;
+#endif // ENABLE_POWER_TOOLS_PP
+      }
+   }
+}
---
+++ SgtCore/PowerFlowSolverParserPlugin.h
@@ -0,0 +1,23 @@
+#ifndef POWER_FLOW_SOLVER_PARSER_PLUGIN_DOT_H
+#define POWER_FLOW_SOLVER_PARSER_PLUGIN_DOT_H
+
+#include <SgtCore/NetworkParser.h>
+
+namespace SmartGridToolbox
+{
+   class Network;
+
+   /// @brief ParserPlugin that parses PowerFlowSolver objects.
+   class PowerFlowSolverParserPlugin : public NetworkParserPlugin
+   {
+      public:
+         virtual const char* key()
+         {
+            return "power_flow_solver";
+         }
+
+         virtual void parse(const YAML::Node& nd, Network& netw, const ParserBase& parser) const override;
+   };
+}
+
+#endif // POWER_FLOW_SOLVER_PARSER_PLUGIN_DOT_H
