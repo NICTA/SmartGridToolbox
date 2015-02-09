3cf833a1e45efb7e049644dc378b6e4dec8f471b
Dan Gordon <dan.gordon@nicta.com.au>
2015-02-09 14:19:39 +1100

    Need to mess with location of config.h.

--- SgtCore/SgtCore.h
+++ SgtCore/SgtCore.h
@@ -18,7 +18,12 @@
 #include <SgtCore/PowerFlow.h>
 #include <SgtCore/PowerFlowSolver.h>
 #include <SgtCore/PowerFlowNrSolver.h>
+#ifdef ENABLE_POWER_TOOLS
 #include <SgtCore/PowerFlowPtSolver.h>
+#endif // ENABLE_POWER_TOOLS
+#ifdef ENABLE_POWER_TOOLS_PP
+#include <SgtCore/PowerFlowPtPpSolver.h>
+#endif // ENABLE_POWER_TOOLS_PP
 #include <SgtCore/Properties.h>
 #include <SgtCore/Random.h>
 #include <SgtCore/SinglePhaseTransformer.h>
