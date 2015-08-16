cee135fbfacf3517853e564e9d685449c4d95fe8
Dan Gordon <dan.gordon@nicta.com.au>
2015-08-17 01:26:19 +1000

    Demo animation for Sylvie.

---
+++ examples/PvDemo/data/anu/modify.m
@@ -0,0 +1,10 @@
+define_constants;
+mpc = loadcase("anu.m");
+mpc.bus(:, QD) *= 5; 
+sel = mpc.branch(:, TAP) == 0;
+mpc.branch(sel, BR_X) *= 5;
+mpc.branch(sel, RATE_A) = 0;
+mpc.branch(sel, RATE_B) = 0;
+mpc.branch(sel, RATE_C) = 0;
+mpc = runpf(mpc);
+savecase("anu_a.m", mpc);
