98ebad83971ee7992baeebe0445fc9f53d543c59
Dan Gordon <dan.gordon@nicta.com.au>
2015-11-05 15:51:15 +1100

    Renaming python stuff.

--- python_interface/setup.py
+++ python_interface/setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup, Extension
 
-libSgtPythonModule = Extension(
-    'libSgtPython', 
-    sources = ['libSgtPython.cc'],
+mod = Extension(
+    'sgt_python', 
+    sources = ['sgt_python.cc'],
     extra_link_args=['-lboost_python3']
 )
 
 setup (name = 'PackageName',
        version = '1.0',
        description = 'SmartGridToolbox + python3',
-       ext_modules = [libSgtPythonModule])
+       ext_modules = [mod])
--- python_interface/sgt_python.cc
+++ python_interface/sgt_python.cc
@@ -7,7 +7,7 @@ char const* greet()
    return "hello, world";
 }
  
-BOOST_PYTHON_MODULE(libSgtPython)
+BOOST_PYTHON_MODULE(sgt_python)
 {
     using namespace boost::python;
     def("greet", greet);
