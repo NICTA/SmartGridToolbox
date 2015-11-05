9783c7000bdfe3e3acd91ef07e20e3791029421f
Dan Gordon <dan.gordon@nicta.com.au>
2015-11-06 10:46:15 +1100

    Rename python_interface -> boost_python and debug.

--- experimental/boost_python/setup.py
+++ experimental/boost_python/setup.py
@@ -4,7 +4,9 @@ mod = Extension(
     'sgt_python', 
     sources = ['sgt_python.cc'],
     extra_compile_args=['-std=c++11'],
-    extra_link_args=['-lboost_python3 -lSgtCore'],
+    extra_link_args=['-lboost_python3', '-lSgtCore', '-lyaml-cpp',
+                     '-lboost_date_time', '-larmadillo', '-lklu', '-lamd',
+                     '-lcolamd', '-lbtf'],
 )
 
 setup (name = 'PackageName',
