ee2d7e7df29729ffdaebe3859a0e166843da3f2c
Dan Gordon <dan.gordon@nicta.com.au>
2015-11-06 14:00:52 +1100

    WIP on python.

--- experimental/boost_python/sgt.cc
+++ experimental/boost_python/sgt.cc
@@ -8,6 +8,23 @@
 
 BOOST_PYTHON_MODULE(sgt)
 {
+    boost::python::enum_<Sgt::Phase>("Phase")
+        .value("BAL", Sgt::Phase::BAL)
+        .value("A", Sgt::Phase::A)
+        .value("B", Sgt::Phase::B)
+        .value("C", Sgt::Phase::C)
+        .value("G", Sgt::Phase::G)
+        .value("N", Sgt::Phase::N)
+        .value("SP", Sgt::Phase::SP)
+        .value("SM", Sgt::Phase::SM)
+        .value("SN", Sgt::Phase::SN)
+        .value("BAD", Sgt::Phase::BAD);
+
+    boost::python::class_<Sgt::Phases>("Phases", boost::python::init<>())
+        .def(boost::python::init<unsigned int>())
+        .def(boost::python::init<Sgt::Phase>())
+        .def(str(boost::python::self));
+
     boost::python::class_<Sgt::Network, boost::noncopyable>("Network", boost::python::init<double>())
         .def(str(boost::python::self));
 }
--- experimental/boost_python/test_sgt.py
+++ experimental/boost_python/test_sgt.py
@@ -1,6 +1,10 @@
 import sgt
 
 def main():
+    print(sgt.Phase.BAL)
+    print(sgt.Phase.A)
+    print(sgt.Phases(sgt.Phase.N))
+    print(sgt.Phases(sgt.Phase.A | sgt.Phase.B | sgt.Phase.N))
     netw = sgt.Network(1)
     print(netw)
 
