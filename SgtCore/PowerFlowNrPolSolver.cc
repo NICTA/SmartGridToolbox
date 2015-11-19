922a4779c9b14efccebc5b0ea93f8f7839270473
Dan Gordon <dan.gordon@nicta.com.au>
2015-11-20 10:52:48 +1100

    WIP, NR polar solver.

--- SgtCore/PowerFlowNrPolSolver.cc
+++ SgtCore/PowerFlowNrPolSolver.cc
@@ -43,6 +43,127 @@ namespace Sgt
             return (Scg + conj(Ic) % M - V % conj(Y * V));
         }
 
+        template<typename T, typename U> auto dM(const T& d, const U& M) -> decltype(diag(d) * M)
+        {
+            using Result = decltype(dM(std::declval<const T&>(), std::declval<const U&>()));
+            Result result = M;
+            for (auto it = result.begin(); it != result.end(); ++it)
+            {
+                *it *= d(it.row());
+            }
+            return result;
+        }
+
+        template<typename T, typename U> auto Md(const T& M, const U& d) -> decltype(M * diag(d))
+        {
+            using Result = decltype(Md(std::declval<const T&>(), std::declval<const U&>()));
+            Result result = M;
+            for (auto it = result.begin(); it != result.end(); ++it)
+            {
+                *it *= d(it.row());
+            }
+            return result;
+        }
+
+        template<typename T, typename U> auto addDiag(const T& M, const U& d) -> decltype(M + diag(d))
+        {
+            using Result = decltype(addDiag(std::declval<const T&>(), std::declval<const U&>()));
+            Result result = M;
+            for (auto it = result.begin(); it != result.end(); ++it)
+            {
+                *it += d(it.row());
+            }
+            return result;
+        }
+
+        template<typename T> SpMat<typename T::elem_type> spDiag(const T& v)
+        {
+            uword n = v.size();
+            Mat<uword> locs(2, n, arma::fill::none);
+            locs.row(0) = linspace<Row<uword>>(0, n - 1, n);
+            locs.row(1) = locs.row(0);
+            return SpMat<typename T::elem_type>(locs, v, false);
+        }
+
+        // Used for the Jacobian. See Matpower's dSbus_dV.
+        // Note however that we're using a negative definition for S compared to matpower.
+        // First returned element is dS_dVm, second is dS_dVa. 
+        std::pair<SpMat<Complex>, SpMat<Complex>> dSdV(const SpMat<Complex>& Y, const Col<Complex>& V)
+        {
+            Col<Complex> I = Y * V;
+            SpMat<Complex> diagI = spDiag(I);
+            SpMat<Complex> diagV = spDiag(V);
+            Col<Complex> VNorm = V / abs(V);
+            SpMat<Complex> diagVNorm = spDiag(VNorm);
+            return {
+                -(diagV * conj(Y * diagVNorm) + conj(diagI) * diagVNorm),
+                -Complex{0, 1} * diagV * conj(diagI - Y * diagV)
+            };
+        }
+        
+        SpMat<double> calcJ1(
+                const uword nPq,
+                const uword nPv,
+                const Col<Complex>& V,
+                const SpMat<Complex>& Y)
+        {
+            uword nPqPv = nPq + nPv;
+
+            auto iP = [&](uword i){return i;};
+            auto iQ = [&](uword i){return i + nPqPv;};
+            auto iM = [&](uword i){return i;};
+            auto iT = [&](uword i){return i + nPq;};
+            auto iPQ = [&](uword i){return i;};
+            auto iPV = [&](uword i){return i + nPq;};
+
+            auto allP = span(iP(0), iP(nPqPv - 1));
+            auto allQ = span(iQ(0), iQ(nPq - 1));
+            auto allM = span(iM(0), iM(nPq - 1));
+            auto allT = span(iT(0), iT(nPqPv - 1));
+            auto allPQ = span(iPQ(0), iPQ(nPq - 1));
+            auto allPQPV = span(iPQ(0), iPV(nPv - 1));
+
+            uword szJ = 2 * nPq + nPv;
+
+            Stopwatch sw;
+            sw.start();
+            auto dSdV_ = dSdV(Y, V);
+            sw.stop();
+            std::cout << "time in dSdV = " << sw.seconds() << std::endl;
+            sw.reset();
+            const auto& dSdM = dSdV_.first;
+            const auto& dSdT = dSdV_.second;
+
+            SpMat<double> dPdM = real(dSdM.submat(allPQPV, allPQ));
+            SpMat<double> dQdM = imag(dSdM.submat(allPQ, allPQ));
+            SpMat<double> dPdT = real(dSdT.submat(allPQPV, allPQPV));
+            SpMat<double> dQdT = imag(dSdT.submat(allPQ, allPQPV));
+            
+            SparseHelper<double> h(szJ, szJ);
+            for (auto it = dPdM.begin(); it != dPdM.end(); ++it)
+            {
+                uword i = it.row(); uword k = it.col(); double val = *it;
+                h.insert(i, k, val);
+            }
+            for (auto it = dQdM.begin(); it != dQdM.end(); ++it)
+            {
+                uword i = it.row(); uword k = it.col(); double val = *it;
+                h.insert(i + nPqPv, k, val);
+            }
+            for (auto it = dPdT.begin(); it != dPdT.end(); ++it)
+            {
+                uword i = it.row(); uword k = it.col(); double val = *it;
+                h.insert(i, k + nPq, val);
+            }
+            for (auto it = dQdT.begin(); it != dQdT.end(); ++it)
+            {
+                uword i = it.row(); uword k = it.col(); double val = *it;
+                h.insert(i + nPqPv, k + nPq, val);
+            }
+
+            return h.get();
+        }
+
         // Build the Jacobian JP for P, theta. Indexing is [0 ... nPqPv - 1].
         SpMat<double> calcJ(
                 const uword nPq,
@@ -56,8 +177,7 @@ namespace Sgt
             arma::Col<double> c = cos(theta);
             arma::Col<double> s = sin(theta);
 
-            SpMat<double> result;
-            SparseHelper<double> helper(3 * nPqPv, 3 * nPqPv);
+            SparseHelper<double> helper(2 * nPq + nPv, 2 * nPq + nPv);
         
             auto cos_ik = [&](uword i, uword k) {return c(i) * c(k) + s(i) * s(k);};
             auto sin_ik = [&](uword i, uword k) {return s(i) * c(k) - c(i) * s(k);};
@@ -77,14 +197,17 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iM(k) == 0) std::cout << "A " << i << " " << k << std::endl;
                     double Gik = *it;
                     if (i != k)
                     {
                         helper.insert(iP(i), iM(k), -M(i) * Gik * cos_ik(i, k));
+                        if (iP(i) == 2 && iM(k) == 0) std::cout << "    A " << -M(i) * Gik * cos_ik(i, k) << " " << cos_ik(i, k) << std::endl;
                     }
                     else
                     {
-                        helper.insert(iP(i), iM(i), -2 * M(i) * Gik);
+                        helper.insert(iP(i), iM(k), -2 * M(i) * Gik);
+                        if (iP(i) == 2 && iM(k) == 0) std::cout << "    B " << -2 * M(i) * Gik << std::endl;
                     }
                 }
             }
@@ -96,9 +219,11 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iM(k) == 0) std::cout << "B " << i << " " << k << std::endl;
                     double Bik = *it;
                     if (i != k)
                     {
+                        if (iP(i) == 2 && iM(k) == 0) std::cout << "    A " << M(i) * Bik * sin_ik(i, k) << std::endl;
                         helper.insert(iP(i), iM(k), M(i) * Bik * sin_ik(i, k));
                     }
                 }
@@ -111,6 +236,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iM(i) == 0) std::cout << "C" << std::endl;
                     double Gil = *it;
                     if (i != l)
                     {
@@ -126,6 +252,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iM(i) == 0) std::cout << "D" << std::endl;
                     double Bil = *it;
                     if (i != l)
                     {
@@ -144,6 +271,7 @@ namespace Sgt
                 {
                     uword i = it.row();
                     uword k = it.col();
+                    if (iP(i) == 2 && iT(k) == 0) std::cout << "E" << std::endl;
                     double Gik = *it;
                     if (i != k)
                     {
@@ -159,6 +287,7 @@ namespace Sgt
                 {
                     uword i = it.row();
                     uword k = it.col();
+                    if (iP(i) == 2 && iT(k) == 0) std::cout << "F" << std::endl;
                     double Bik = *it;
                     if (i != k)
                     {
@@ -174,6 +303,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iT(i) == 0) std::cout << "G" << std::endl;
                     double Gil = *it;
                     if (i != l)
                     {
@@ -189,6 +319,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iP(i) == 2 && iT(i) == 0) std::cout << "H" << std::endl;
                     double Bil = *it;
                     if (i != l)
                     {
@@ -207,6 +338,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iM(k) == 0) std::cout << "I" << std::endl;
                     double Gik = *it;
                     if (i != k)
                     {
@@ -222,6 +354,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iM(k) == 0) std::cout << "J" << std::endl;
                     double Bik = *it;
                     if (i != k)
                     {
@@ -229,7 +362,7 @@ namespace Sgt
                     }
                     else
                     {
-                        helper.insert(iQ(i), iM(i), 2 * M(i) * Bik);
+                        helper.insert(iQ(i), iM(k), 2 * M(i) * Bik);
                     }
                 }
             }
@@ -241,6 +374,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iM(i) == 0) std::cout << "K" << std::endl;
                     double Gil = *it;
                     if (i != l)
                     {
@@ -256,6 +390,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iM(i) == 0) std::cout << "L" << std::endl;
                     double Bil = *it;
                     if (i != l)
                     {
@@ -274,6 +409,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iT(k) == 0) std::cout << "M" << std::endl;
                     double Gik = *it;
                     if (i != k)
                     {
@@ -289,6 +425,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword k = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iT(k) == 0) std::cout << "N" << std::endl;
                     double Bik = *it;
                     if (i != k)
                     {
@@ -304,6 +441,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iT(i) == 0) std::cout << "O" << std::endl;
                     double Gil = *it;
                     if (i != l)
                     {
@@ -319,6 +457,7 @@ namespace Sgt
                 {
                     uword i = it.row(); // Index in full set of busses.
                     uword l = it.col(); // Index in full set of busses.
+                    if (iQ(i) == 2 && iT(i) == 0) std::cout << "P" << std::endl;
                     double Bil = *it;
                     if (i != l)
                     {
@@ -361,7 +500,6 @@ namespace Sgt
         uword nPqPv = nPq + nPv;
 
         const SpMat<Complex>& Y = mod_->Y(); // Model indexing. Includes shunts (const Y in ZIPs).
-        sgtLogDebug() << "Y = \n" << Y << std::endl;
         SpMat<double> G = real(Y); // Model indexing. Includes shunts (const Y in ZIPs).
         SpMat<double> B = imag(Y); // Model indexing. Includes shunts (const Y in ZIPs).
 
@@ -386,12 +524,8 @@ namespace Sgt
             LogIndent _;
 
             auto S = calcS(Scg, Ic, V, M, Y);
-            auto f = join_vert(real(S.subvec(0, nPqPv - 1)), imag(S.subvec(0, nPq - 1)));
+            arma::Col<double> f = join_vert(real(S.subvec(0, nPqPv - 1)), imag(S.subvec(0, nPq - 1)));
             
-            sgtLogDebug() << "M = " << M << std::endl;
-            sgtLogDebug() << "theta = " << theta << std::endl;
-            sgtLogDebug() << "S = " << S << std::endl;
-
             err = norm(f, "inf");
             sgtLogMessage(LogLevel::VERBOSE) << "Err = " << err << std::endl;
             if (err <= tol_) 
@@ -401,21 +535,37 @@ namespace Sgt
             }
 
             Col<double> x; // Delta theta.
+            Stopwatch sw;
+            sw.start();
             SpMat<double> J = calcJ(nPq, nPv, M, theta, G, B);
+            sw.stop();
+            std::cout << "J " << sw.seconds() << std::endl;
+            sw.reset();
+            sw.start();
+            SpMat<double> J1 = calcJ1(nPq, nPv, V, Y);
+            sw.stop();
+            std::cout << "J1 " << sw.seconds() << std::endl;
 
-            ok = solveSparseSystem(J, f, x);
+            ok = solveSparseSystem(J1, f, x);
             if (!ok)
             {
                 sgtLogWarning() << "Solve failed." << std::endl;
                 break;
             }
+            sgtLogDebug() << "--------------------" << std::endl;
+            sgtLogDebug() << "V = " << V << std::endl;
+            sgtLogDebug() << "S = " << S << std::endl;
+            sgtLogDebug() << "f = " << f << std::endl;
+            sgtLogMessage() << "|f| = " << err << std::endl;
             sgtLogDebug() << "x = " << x << std::endl;
+            sgtLogDebug() << "J =\n" << std::setw(10) << arma::Mat<double>(J) << std::endl;
+            sgtLogDebug() << "J1 =\n" << std::setw(10) << arma::Mat<double>(J1) << std::endl;
+            sgtLogDebug() << "--------------------" << std::endl;
 
             // Update M, theta, V.
             M.subvec(0, nPq - 1) += x.subvec(0, nPq - 1);
             theta.subvec(0, nPqPv - 1) += x.subvec(nPq, 2 * nPq + nPv - 1);
-            Col<Complex> dir = cx_vec(cos(theta), sin(theta)); 
-            V.subvec(0, nPqPv - 1) = M.subvec(0, nPqPv - 1) % dir.subvec(0, nPqPv - 1);
+            V.subvec(0, nPqPv - 1) = M.subvec(0, nPqPv - 1) % exp(Complex{0, 1} * theta.subvec(0, nPqPv - 1));
         }
 
         if (!wasSuccessful)
