d4eec2e03466ddab13831319a2cfc43c04f7594e
Dan Gordon <dan.gordon@nicta.com.au>
2016-01-14 09:35:32 +1100

    Formatting sgt_client.

--- experimental/SgtClient/js/sgt_client.js
+++ experimental/SgtClient/js/sgt_client.js
@@ -77,25 +77,33 @@ function drawGraph(netw) {
 
 var selector = $("#select_matpower");
 var files = $.getJSON('http://localhost:34568/matpower_files/', function(files) {
-        for (i = 0; i < files.length; ++i)
-        {
+    for (i = 0; i < files.length; ++i)
+    {
         selector.append("<option>" + files[i] + '</option>');
-        }
-        });
+    }
+});
 
 selector.change(
-        function() {
+    function() {
         var file = $(this).find("option:selected").text();
         var url = 'http://localhost:34568/networks/' + file;
         var json = JSON.stringify({"matpower_filename": file});
         $.ajax({
-url: url,
-type: 'PUT',
-contentType: "application/json",
-data: json,
-success: function(result) {
-loadNetwork(file);
-}
-});
+            url: url,
+            type: 'PUT',
+            contentType: "application/json",
+            data: json,
+            success: function(result) {
+                loadNetwork(file);
+            }
+        });
+    }
+);
+
+document.body.addEventListener('keydown', function (e) {
+    if (renderer) {
+        if (e.which === 32) { // spacebar
+            renderer.reset();
         }
-        );
+    }
+});
