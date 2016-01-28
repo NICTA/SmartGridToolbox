745553ecf996cd1834d86a565a07c727b3dd1560
Dan Gordon <dan.gordon@nicta.com.au>
2016-01-28 16:13:51 +1100

    WIP on webgl colormap.

--- experimental/SgtClient/webgl-tests/webgl-demo.js
+++ experimental/SgtClient/webgl-tests/webgl-demo.js
@@ -1,4 +1,4 @@
-var N = 16;
+var N = 64;
 var nTri = 2 * (N - 1) * (N - 1);
 var nInd = 3 * nTri;
 
@@ -9,6 +9,8 @@ var vertexIndicesBuffer;
 var verticesColorBuffer;
 var vertexPositionAttribute;
 var shaderProgram;
+
+var dat;
 
 //
 // start
@@ -23,10 +25,14 @@ function start() {
     // Only continue if WebGL is available and working
 
     if (gl) {
-        gl.clearColor(0.9, 0.9, 1.0, 1.0);  // Clear to black, fully opaque
+        gl.enable(gl.BLEND);
+        gl.blendEquation(gl.FUNC_ADD);
+        gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
+        gl.clearColor(1, 1, 1, 1);  // Clear to black, fully opaque
         gl.clearDepth(1.0);                 // Clear everything
 
         initShaders();
+        initData();
         initBuffers();
         drawScene();
     }
@@ -46,15 +52,74 @@ function initWebGL() {
     }
 }
 
+function initData() {
+    function rand(min, max)
+    {
+        return min + Math.random() * (max - min);
+    }
+
+    var nDat = 10;
+    dat = [];
+    for (var i = 0; i < nDat; ++i)
+    {
+        dat.push([rand(-1, 1), rand(-1, 1), rand(0, 1)]);
+    }
+}
+
 function initBuffers() {
+    var spreadVal = 0.1;
+    var spreadSat = 0.1;
+
+    function idx(i, j) {
+        return i * N + j;
+    }
+    
+    function disp(p1, p2) {
+        return [p1[0] - p2[0], p1[1] - p2[1]];
+    }
+    
+    function manhattan(disp) {
+        return Math.max(Math.abs(disp[0]), Math.abs(disp[1]));
+    }
+
+    function weight(d, s)
+    {
+        var dds = [d[0] / s, d[1] / s];
+        var l2 = dds[0] * dds[0] + dds[1] * dds[1];
+        return 1.0 / (1.0 + l2); // Cauchy distribution.
+    }
+
     var vertices = [];
+    var vertColorWork = [];
     var verticesColor = [];
     for (var i = 0; i < N; ++i) {
         var x = 2 * i / (N - 1) - 1;
         for (var j = 0; j < N; ++j) {
             var y = 2 * j / (N - 1) - 1;
             vertices.push.apply(vertices, [x, y, 0.0]);
-            verticesColor.push.apply(verticesColor, [0.5 * (x + 1), 0.5 * (y + 1), Math.random(), 1.0]);
+
+            var totWeightVal = 0.0;
+            var totWeightSat = 0.0;
+            var val = 0.0;
+            
+            for (var k = 0; k < dat.length; ++k) {
+                var datPos = dat[k].slice(0, 2);
+                var datVal = dat[k][2];
+                var d = disp([x, y], datPos);
+                // if (manhattan(d) < 0.25) {
+                    var wVal = weight(d, spreadVal);
+                    totWeightVal += wVal;
+                    val += wVal * datVal;
+                    totWeightSat += weight(d, spreadSat);
+                // }
+            }
+
+            if (totWeightVal > 0.0) {
+                val /= totWeightVal;
+            }
+            var sat = 1.0 - Math.exp(-totWeightSat * totWeightSat);
+
+            verticesColor.push.apply(verticesColor, [val, val, val, sat]);
         }
     }
 
@@ -67,9 +132,6 @@ function initBuffers() {
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticesColor), gl.STATIC_DRAW);
 
     var vertexIndices = [];
-    function idx(i, j) {
-        return i * N + j;
-    }
     for (var i = 0; i < N - 1; ++i) {
         for (var j = 0; j < N - 1; ++j) {
             vertexIndices.push.apply(
