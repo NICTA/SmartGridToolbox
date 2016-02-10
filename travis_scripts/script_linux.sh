803181d46a588623c7cad76a85c879e7679098a2
Dan Gordon <dan.gordon@nicta.com.au>
2016-02-10 13:52:45 +1100

    Working on travis build.

--- travis_scripts/script_linux.sh
+++ travis_scripts/script_linux.sh
@@ -2,8 +2,9 @@
 
 cd third_party
 ./install_third_party.sh $INSTALL_CXX $HOME || exit $?
+cd ..
 
 ./autogen.sh
 ./configure CXX=$INSTALL_CXX CPPFLAGS=-I$HOME/include LDFLAGS=-L$HOME/lib --with-klu || exit $?
-make || exit $?
+make $TARGS || exit $?
 make check
