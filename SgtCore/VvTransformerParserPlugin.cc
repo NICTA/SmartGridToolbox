49b2c9089b03a5bfe396ec0749a7691cd97a38b4
Dan Gordon <dan.gordon@nicta.com.au>
2016-05-13 16:08:40 +1000

    Adding parser for VvTransformers.

---
+++ SgtCore/VvTransformerParserPlugin.cc
@@ -0,0 +1,58 @@
+// Copyright 2015 National ICT Australia Limited (NICTA)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "VvTransformerParserPlugin.h"
+
+#include "VvTransformer.h"
+#include "Network.h"
+#include "YamlSupport.h"
+
+namespace Sgt
+{
+    void VvTransformerParserPlugin::parse(const YAML::Node& nd, Network& netw, const ParserBase& parser) const
+    {
+        auto trans = parseVvTransformer(nd, parser);
+
+        assertFieldPresent(nd, "bus_0_id");
+        assertFieldPresent(nd, "bus_1_id");
+
+        std::string bus0Id = parser.expand<std::string>(nd["bus_0_id"]);
+        std::string bus1Id = parser.expand<std::string>(nd["bus_1_id"]);
+
+        netw.addBranch(std::move(trans), bus0Id, bus1Id);
+    }
+
+    std::unique_ptr<VvTransformer> VvTransformerParserPlugin::parseVvTransformer(const YAML::Node& nd,
+            const ParserBase& parser) const
+    {
+        assertFieldPresent(nd, "id");
+        assertFieldPresent(nd, "nom_ratio");
+        assertFieldPresent(nd, "leakage_impedance");
+
+        const std::string id = parser.expand<std::string>(nd["id"]);
+        Complex nomRatio = parser.expand<Complex>(nd["nom_ratio"]);
+
+        auto ndOffNomRatio13 = nd["off_nom_ratio_13"];
+        Complex offNomRatio13 = ndOffNomRatio13 ? parser.expand<Complex>(nd["off_nom_ratio_13"]) : 1.0;
+        
+        auto ndOffNomRatio23 = nd["off_nom_ratio_23"];
+        Complex offNomRatio23 = ndOffNomRatio23 ? parser.expand<Complex>(nd["off_nom_ratio_23"]) : 1.0;
+
+        Complex ZL = parser.expand<Complex>(nd["leakage_impedance"]);
+
+        std::unique_ptr<VvTransformer> trans(new VvTransformer(id, nomRatio, offNomRatio13, offNomRatio23, ZL));
+
+        return trans;
+    }
+}
---
+++ SgtCore/VvTransformerParserPlugin.h
@@ -0,0 +1,41 @@
+// Copyright 2015 National ICT Australia Limited (NICTA)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VV_TRANSFORMER_PARSER_PLUGIN
+#define VV_TRANSFORMER_PARSER_PLUGIN
+
+#include <SgtCore/NetworkParser.h>
+
+namespace Sgt
+{
+    class VvTransformer;
+    class Network;
+
+    /// @brief ParserPlugin that parses VvTransformer objects.
+    /// @ingroup PowerFlowCore
+    class VvTransformerParserPlugin : public NetworkParserPlugin
+    {
+        public:
+            virtual const char* key() override
+            {
+                return "v_v_transformer";
+            }
+
+            virtual void parse(const YAML::Node& nd, Network& netw, const ParserBase& parser) const override;
+
+            std::unique_ptr<VvTransformer> parseVvTransformer(const YAML::Node& nd, const ParserBase& parser) const;
+    };
+}
+
+#endif // VV_TRANSFORMER_PARSER_PLUGIN
