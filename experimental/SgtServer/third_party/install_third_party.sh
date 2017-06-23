a754bee72b113138545fcca3ed6b343db0ffd2c0
Dan Gordon <dan.gordon@anu.edu.au>
2017-06-23 14:01:23 +1000

    Don't provide source for cpprestsdk.
    It's available on homebrew and apt-get.

--- experimental/SgtServer/third_party/install_third_party.sh
+++
@@ -1,8 +0,0 @@
-cd cpprestsdk
-rm -rf build_sgt
-mkdir build_sgt
-cd build_sgt
-cmake -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF ../Release || exit $?
-make || exit $?
-sudo make install || exit $?
-cd ../..
