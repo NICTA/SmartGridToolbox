b1a16c0d7171c030596031c7689a61b417c2ef4e
Dan Gordon <dan.gordon@anu.edu.au>
2018-05-25 10:54:02 +1000

    Fix up simulation tutorial.

--- tutorials/02_simulation/simulation.cc
+++ tutorials/02_simulation/simulation.cc
@@ -19,41 +19,47 @@ int main(int argc, char** argv)
 {
     using namespace Sgt;
     
-    Simulation sim; // The top level simulation object.
+    Simulation sim;
+        // The top level simulation object.
     Parser<Simulation> p;
-    // Parse information into sim from config file "simulation.yaml".
+        // Parse information into sim from config file "simulation.yaml".
     p.parse("simulation.yaml", sim);
     
-    // We just parsed in a simulation. The user is also free to modify this
-    // programmatically, e.g. below we add an extra zip load to the network.
-    auto netw = sim.simComponent<SimNetwork>("network"); // Get network by name.
-    auto extraZip = std::make_shared<GenericZip>("extra_zip", Phase::BAL); // Make a GenericZip.
-    extraZip->setYConst({1e-3}); // Modify it.
-    netw->network().addZip(extraZip, "bus_2");
-    auto simExtraZip = sim.newSimComponent<SimZip>(extraZip->id(), *extraZip); 
+        // We just parsed in a simulation. The user is also free to modify this programmatically, e.g. below we add
+        // an extra zip load to the network.
+    auto simNetw = sim.simComponent<SimNetwork>("network");
+        // Get network by name.
+    auto extraZip = std::make_shared<Zip>("extra_zip", Phase::BAL);
+        // Make a Zip.
+    extraZip->setYConst({1e-3});
+        // Modify it: constant impedance load.
+    auto extraZipCompPtr = simNetw->network().addZip(extraZip, "bus_2");
+        // Add it to the network, returning a ComponentPtr to the component.
+    auto simExtraZip = sim.newSimComponent<SimZip>(extraZipCompPtr); 
         // Give it to a new SimZip for the Simulation.
-    simExtraZip->linkToSimNetwork(*netw); // Add it to the SimNetwork.
 
-    // After parsing and other user setup is done, we need to call initialize.
-    // All components will be initialized to time sim.startTime().
+        // After parsing or making other modifications, we need to call initialize. All components will be initialized
+        // to time sim.startTime().
     sim.initialize();
 
     Log().message() << *sim.simComponent<SimNetwork>("network") << std::endl;
+        // Print the network, at the start of the simulation.
 
-    auto bus2 = sim.simComponent<SimBus>("bus_2"); // Get bus by name.
+    auto bus2 = simNetw->network().buses()["bus_2"];
+        // Get bus by name.
 
     while (!sim.isFinished())
     {
         Log().message() << "Timestep " << sim.currentTime() << std::endl;
         sim.doTimestep();
-            // Perform a single timestep: keep updating components until
-            // (1) the time has increased and all components are up to date at
-            // that time, or (2) the simulation finishes.
-            // Finer grained control is also possible, e.g. sim.doNextUpdate()
-            // will update one component at a time.
+            // Perform a single timestep: keep updating components until (1) the time has increased and all components
+            // are up to date at that time, or (2) the simulation finishes. Finer grained control is also possible,
+            // e.g. sim.doNextUpdate() will update one component at a time.
         {
-            sgtLogIndent(); // Indent within scope.
-            Log().message() << "bus_2 V = " << bus2->bus().V() << std::endl;
+            sgtLogIndent();
+                // Indent within scope.
+            Log().message() << "bus_2 V = " << bus2->V() << std::endl;
+                // Log the voltage at the current time.
         }
     }
 }
