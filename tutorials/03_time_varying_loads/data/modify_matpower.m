00c819012f3d90346417d2ad37c999612b8e5436
Dan Gordon <dan.gordon@anu.edu.au>
2018-05-25 11:30:39 +1000

    Tutorial 3 now works.

--- tutorials/03_time_varying_loads/data/modify_matpower.m
+++
@@ -1,9 +0,0 @@
-function loads = aggregate_loads(case_rel_path)
-    define_constants;
-    mpc = loadcase([pwd(), '/', case_rel_path]);
-    mpc.bus(:, PD) = 0.0;
-    mpc.bus(:, QD) = 0.0;
-    [dirname, fname, ext] = fileparts(case_rel_path);
-    fname_mod = fullfile(dirname, [fname, '_mod', ext]);
-    savecase(fname_mod, mpc);
-end
