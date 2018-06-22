6a1167f20ea0e4f5e7d69d8dd3d4c4bf7dfe45a2
Dan Gordon <dan.gordon@anu.edu.au>
2018-06-22 15:55:46 +1000

    Work on docs.

--- docs/doxygen-doc/html/search/groups_a.js
+++ docs/doxygen-doc/html/search/groups_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['yaml_20specification_20for_20networks',['YAML specification for Networks',['../group___network_yaml_spec.html',1,'']]],
-  ['yaml_20specification_20for_20simulations',['YAML specification for Simulations',['../group___sim_yaml_spec.html',1,'']]]
+  ['yaml_20specification_20for_20simulations',['YAML specification for Simulations',['../group___sim_yaml_spec.html',1,'']]],
+  ['yaml_20configuration_20file_20specs',['YAML Configuration File Specs',['../group___yaml_spec.html',1,'']]]
 ];
