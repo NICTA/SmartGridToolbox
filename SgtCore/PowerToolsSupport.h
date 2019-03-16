09e767f98bffc94d648b51bbf01e2aa0366d9934
Dan Gordon <dan.gordon@anu.edu.au>
2019-03-16 11:39:12 +1100

    Add ANU's IP to the copyright.

--- SgtCore/PowerToolsSupport.h
+++ SgtCore/PowerToolsSupport.h
@@ -1,4 +1,5 @@
-// Copyright 2015 National ICT Australia Limited (NICTA)
+// Copyright 2015-2016 National ICT Australia Limited (NICTA)
+// Copyright 2016-2019 The Australian National University
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
